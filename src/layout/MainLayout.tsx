bee05e438fb8d521aaf4e5024ac86bea91ae75ef
andreasholst <andreasholst1997@gmail.com>
2024-11-18 11:50:52 +0200

    chore: create necessary backbone for login functionality add Login component refactor MainLayout refactor HomePage add Hero component add placeholder Navbar add code to .prettierrc

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -1,19 +1,14 @@
 import { Outlet } from "react-router-dom";
 import { motion } from "motion/react";
+import Navbar from "../components/Navbar"
 //import Navbar from "../components/Navbar";
 
 const MainLayout = () => {
   return (
-    <div className="dark h-screen w-screen bg-background text-text">
-      <motion.button
-        whileHover={{ scale: 1.1 }}
-        whileTap={{ scale: 0.95 }}
-        className="m-4 rounded-lg bg-primary-500 p-4 text-text transition-colors hover:bg-primary-400"
-      >
-        Button
-      </motion.button>
+    <>
+      <Navbar />
       <Outlet />
-    </div>
+    </>
   );
 };
 
