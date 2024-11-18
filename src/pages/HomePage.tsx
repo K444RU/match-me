68b004a8822a366acb41d411d80b0b31d35ccba6
Karl Romet Somelar <karlromets@gmail.com>
2024-11-18 15:45:47 +0200

    chore: removing unused code & values

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,28 +1,12 @@
 import { useRef } from 'react';
 import Hero from '../components/Hero';
 import HowItWorksSection from '../sections/HowItWorksSection';
-import { useState, useEffect } from 'react';
 const HomePage = () => {
-  const howItWorksRef = useRef<HTMLDivElement | null>(null);
+  const howItWorksRef = useRef<HTMLDivElement>(null);
 
   const scrollToHowItWorks = () => {
     howItWorksRef.current?.scrollIntoView({ behavior: 'smooth' });
   };
-
-  const [scrollPosition, setScrollPosition] = useState(0);
-  const handleScroll = () => {
-    const position = window.scrollY;
-    setScrollPosition(position);
-  };
-  
-  useEffect(() => {
-    window.addEventListener('scroll', handleScroll, { passive: true });
-    
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
-  console.log(scrollPosition);
   
   return (
     <>
--- src/sections/HowItWorksSection.tsx
+++ src/sections/HowItWorksSection.tsx
@@ -1,6 +1,6 @@
 import { forwardRef } from 'react';
 
-const HowItWorksSection = forwardRef<HTMLDivElement>((props, ref) => {
+const HowItWorksSection = forwardRef<HTMLDivElement>((_, ref) => {
     return (
       <div ref={ref} className="min-h-screen bg-background-50 py-20">
         HowItWorksSection
