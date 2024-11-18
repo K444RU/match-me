2362ddb7408c9c603a2a1c973870189d2e706629
Karl Romet Somelar <karlromets@gmail.com>
2024-11-18 14:17:18 +0200

    feat: add "Learn More" button functionality
    add section dir & HowItWorksSection
    add smoothScrolling scrollTo for HomePage

---
+++ src/sections/HowItWorksSection.tsx
@@ -0,0 +1,11 @@
+import { forwardRef } from 'react';
+
+const HowItWorksSection = forwardRef<HTMLDivElement>((props, ref) => {
+    return (
+      <div ref={ref} className="min-h-screen bg-background-50 py-20">
+        HowItWorksSection
+      </div>
+    );
+  });
+
+export default HowItWorksSection
