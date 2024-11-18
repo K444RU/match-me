b2657089802a8ceafe4e325f0bd9eb720fff57ba
Karl Romet Somelar <karlromets@gmail.com>
2024-11-18 12:47:04 +0200

    chore: remove unused imports

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -1,7 +1,5 @@
 import { Outlet } from "react-router-dom";
-import { motion } from "motion/react";
 import Navbar from "../components/Navbar"
-//import Navbar from "../components/Navbar";
 
 const MainLayout = () => {
   return (
