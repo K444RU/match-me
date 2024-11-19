cb3f50b8e48da486b386d4e6a707d78fbea2427d
Karl Romet Somelar <karlromets@gmail.com>
2024-11-19 20:10:40 +0200

    feat: add backend jwt generation and password hashing

---
+++ back-end/src/main/java/com/matchme/srv/security/services/UserDetailsServiceImpl.java
@@ -0,0 +1,27 @@
+package com.matchme.srv.security.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.matchme.srv.user.User;
+import com.matchme.srv.user.UserRepository;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with email: " + email));
+
+        return UserDetailsImpl.build(user);
+    }
+
+}
---
+++ back-end/src/main/java/com/matchme/srv/user/UserRepository.java
@@ -0,0 +1,12 @@
+package com.matchme.srv.user;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByEmail(String email);
+
+    Boolean existsByEmail(String email);
+
+}
