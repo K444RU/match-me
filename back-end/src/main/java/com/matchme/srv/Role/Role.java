3d0ce46dfbd3269772d23515b4a39c442bdb7ed2
Karl Romet Somelar <karlromets@gmail.com>
2024-11-19 23:03:44 +0200

    refactor: add lombok

--- back-end/src/main/java/com/matchme/srv/Role/Role.java
+++ back-end/src/main/java/com/matchme/srv/Role/Role.java
@@ -11,6 +11,9 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
+import lombok.Data;
+
+@Data
 @Entity
 @Table(name = "roles")
 public class Role {
@@ -21,28 +24,4 @@ public class Role {
     @Enumerated(EnumType.STRING)
     @Column(length = 20)
     private ERole name;
-
-    public Role() {
-
-    }
-
-    public Role(ERole name) {
-        this.name = name;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public ERole getName() {
-        return name;
-    }
-
-    public void setName(ERole name) {
-        this.name = name;
-    }
 }
