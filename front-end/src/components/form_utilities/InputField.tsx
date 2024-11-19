5ee2184d2299bbe10e62aca15843a3daf98a952a
HolstAndreas <166335551+HolstAndreas@users.noreply.github.com>
2024-11-19 17:04:21 +0200

    chore: added inputfield backbone and moving to shared workingspace

--- front-end/src/components/form_utilities/InputField.tsx
+++ front-end/src/components/form_utilities/InputField.tsx
@@ -1,6 +1,9 @@
-const InputField = () => {
+const InputField = ({ label }: { label: string }, { htmlFor }: { htmlFor: string }) => {
   return (
-    <div>InputField</div>
+    <div className="mb-3 place-items-start">
+      <label className="ml-2" htmlFor={`${htmlFor}`}>{label}</label>
+
+    </div>
   )
 }
 
