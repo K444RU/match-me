9babe65ae252d3f20e40b08e3bb6eed9e8ba6730
Karl Romet Somelar <karlromets@gmail.com>
2024-11-20 12:00:49 +0200

    feat: add exception handler
    now we actually see errors...

--- back-end/src/main/java/com/matchme/srv/payload/request/SignupRequest.java
+++ back-end/src/main/java/com/matchme/srv/payload/request/SignupRequest.java
@@ -8,16 +8,16 @@ import lombok.Setter;
 @Setter
 public class SignupRequest {
 
-  @NotBlank
-  @Size(max = 50)
-  @Email
+  @NotBlank(message = "Email cannot be empty")
+  @Size(max = 320, message = "Email must be less than 320 characters")
+  @Email(message = "Email must be valid")
   private String email;
 
-  @NotBlank
+  @NotBlank(message = "Phone number cannot be empty")
   @Size(max = 20)
   private String number;
 
-  @NotBlank
-  @Size(min = 6, max = 40)
+  @NotBlank(message = "Password cannot be empty")
+  @Size(min = 6, max = 40, message = "Password must be between 6 and 40 characters")
   private String password;
 }
