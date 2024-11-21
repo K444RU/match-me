4b94480f661d106f22ad622f8e1e879317809152
Karl Romet Somelar <karlromets@gmail.com>
2024-11-21 14:10:54 +0200

    feat: add proper auth w/ java backend w/ axios

--- front-end/src/components/LoginForm.tsx
+++ front-end/src/components/LoginForm.tsx
@@ -2,12 +2,20 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import MotionSpinner from './motion/MotionSpinner';
 import InputField from './form_utilities/InputField';
+import axios from 'axios';
+import FormResponse from './form_utilities/FormResponse';
 
-const LoginForm = ({ setShowOverlay }: { setShowOverlay: (show: boolean) => void }) => {
+const LoginForm = ({
+  setShowOverlay,
+}: {
+  setShowOverlay: (show: boolean) => void;
+}) => {
   const navigate = useNavigate();
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('admin@kood.tech');
+  const [password, setPassword] = useState('123456');
   const [loading, setLoading] = useState(false);
+  const [resTitle, setResTitle] = useState('');
+  const [resSubtitle, setResSubtitle] = useState('');
 
   interface User {
     email: string;
@@ -23,80 +31,64 @@ const LoginForm = ({ setShowOverlay }: { setShowOverlay: (show: boolean) => void
       password,
     };
 
-    console.log(user);
-    authenticate(user);
-  };
-
-  const authenticate = async (user: User) => {
-    try {
-      const res = await fetch('/api/users?');
-      const data = await res.json();
-      // TEMP:
-      // we send username and password, use that on serverside to find user
-      // and match password, but here we must temporarily find on client-side
-      interface db_user {
-        id: number;
-        username: string;
-        email: string;
-        phone: string;
-        password: string;
-      }
-
-      // TODO:
-      // Show adequate UI display for incorrect password or username not
-      // found
-      const matchedUser = data.find(
-        (db_user: db_user) => db_user.email === user.email
-      );
-      if (!matchedUser) {
-        setLoading(false);
-        console.error('No user found.');
-        return;
-      }
-      if (matchedUser.password === user.password) {
+    axios
+      .post('/api/auth/signin', {
+        email: user.email,
+        password: user.password,
+      })
+      .then((res) => {
+        if (res.data.token) {
+          localStorage.setItem('user', JSON.stringify(res.data));
+        }
+        console.log(res);
         navigate('/chats');
         setShowOverlay(false);
-      } else {
-        console.error('Invalid Password');
-      }
-      //
-
-      // PRODUCTION:
-      // We send email and password to backend, if success: true, we return
-      // a boolean of true. so if authenticate(user) === true, then login.
-    } catch (error) {
-      console.error('Error fetching data', error);
-      throw error;
-    }
-    setLoading(false);
+      })
+      .catch((err) => {
+        if (err.response.status === 401) {
+          setResTitle('Wrong Credentials');
+          setResSubtitle('Invalid username or password');
+        } else {
+          setResTitle('Something went wrong...');
+          setResSubtitle('Please contact support.');
+        }
+        console.error('Error logging in:', err);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
   };
 
   return (
-    <form onSubmit={submitForm}>
+    <form onSubmit={submitForm} className='flex flex-col gap-3 items-center'>
+      {resTitle && resSubtitle && (
+        <FormResponse title={resTitle} subtitle={resSubtitle} />
+      )}
       <InputField
-        label="Email address"
+        // label="Email address"
         type="email"
         name="contact_email"
-        placeholder="Enter email"
+        placeholder="Email"
         value={email}
         onChange={setEmail}
         required={true}
       />
       <InputField
-        label="Password"
+        // label="Password"
         type="text"
         name="password"
-        placeholder="Enter password"
+        placeholder="Password"
         value={password}
         onChange={setPassword}
         required={true}
       />
       <button
-        className="rounded-md bg-primary px-5 py-2 mt-3 text-text hover:bg-primary-200 hover:text-text"
+        className="rounded-md bg-primary px-5 py-2 text-text hover:bg-primary-200 hover:text-text self-start w-full font-semibold tracking-wide transition-colors flex justify-center gap-2 items-center"
         type="submit"
         aria-label="Submit form."
       >
-        {loading ? <MotionSpinner /> : 'Log in'}
+        <span>Login</span>
+        {loading && <MotionSpinner/>}
       </button>
     </form>
   );
--- front-end/src/components/RegisterForm.tsx
+++ front-end/src/components/RegisterForm.tsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import InputField from './form_utilities/InputField';
 import MotionSpinner from './motion/MotionSpinner';
 import axios from 'axios';
+import FormResponse from './form_utilities/FormResponse';
 
 const RegisterForm = ({
   setShowOverlay,
@@ -15,6 +16,9 @@ const RegisterForm = ({
   const [countryCode, setCountryCode] = useState('+372');
   const [password, setPassword] = useState('123456');
   const [loading, setLoading] = useState(false);
+  const [resTitle, setResTitle] = useState('');
+  const [resSubtitle, setResSubtitle] = useState('');
+  const [resState, setResState] = useState<'error' | 'success'>('error');
 
   interface User {
     email: string;
@@ -31,46 +35,86 @@ const RegisterForm = ({
       password,
     };
 
-    axios.post('/api/auth/signup', {
-      email: newUser.email,
-      number: newUser.phone,
-      password: newUser.password,
-    })
-    .then((res) => {
-      navigate('/finish-profile');
-      setShowOverlay(false);
-      console.log(res);
-    })
-    .catch((err) => {
-      console.error('Error registering user:', err);
-    }).finally(() => {
-      setLoading(false);
-    });
+    axios
+      .post('/api/auth/signup', {
+        email: newUser.email,
+        number: newUser.phone,
+        password: newUser.password,
+      })
+      .then((res) => {
+        // TODO: Don't redirect on register & wait for email verify.
+        // This current approach would cause a unnecessary
+        // waste of resources (unverified accounts in DB)
+        // So on successful registration we indicate 'you have been registered'
+        // and then under that 'Please verify your email.'
+        // Email verification allows login and redirects to /profile
+        // /profile should be smarter, instead of using /finish-profile we just use the main profile page
+        // if user has missing fields, he cannot proceed after fields are finished.
+        // otherwise it is just the normal /profile page.
+        // navigate('/profile');
+        // setShowOverlay(false);
+        console.log(res);
+        setResState('success');
+        setResTitle('Nice! You have been registered.');
+        setResSubtitle('Please verify your email.');
+      })
+      .catch((err) => {
+        console.error('Error registering user:', err);
+        if (err.response.status === 401) {
+          setResTitle('Wrong Credentials');
+          setResSubtitle('Invalid username or password');
+        } else if (err.response.status === 400) {
+          setResTitle('We found some errors');
+          setResState('error');
+          setResSubtitle('');
+          // TODO: Make back-end return better error messages
+          // size must be between 0 and 20 -> what size? (its number...)
+          // Password must be between 6 and 40 characters -> good
+          // Error: email is already taken!
+          Object.keys(err.response.data).forEach((key) => {
+            setResSubtitle((prev) => `${prev}\n${err.response.data[key]}`);
+          });
+        } else {
+          setResTitle('Something went wrong...');
+          setResSubtitle('Please contact support.');
+        }
+      })
+      .finally(() => {
+        setLoading(false);
+      });
   };
 
   return (
-    <form onSubmit={submitForm}>
+    <form onSubmit={submitForm} className="flex flex-col items-center gap-3">
+      {resTitle && resSubtitle && (
+        <FormResponse
+          title={resTitle}
+          subtitle={resSubtitle}
+          state={resState}
+        />
+      )}
       <InputField
-        label="Email address"
+        // label="Email address"
         type="email"
         name="contact_email"
-        placeholder="Enter email"
+        placeholder="Email"
         value={email}
         onChange={setEmail}
         required={true}
       />
       <InputField
-        label="Password"
+        // label="Password"
         type="text"
         name="password"
-        placeholder="Enter password"
+        placeholder="Password"
         value={password}
         onChange={setPassword}
         required={true}
       />
-      <div className="mb-3 flex place-items-start space-x-2">
-        <InputField className="w-20"
-          label="Country"
+      <div className="flex place-items-start space-x-2">
+        <InputField
+          className="w-24"
+          // label="Country"
           type="text"
           name="country_code"
           placeholder="+372"
@@ -79,22 +123,22 @@ const RegisterForm = ({
           required={true}
         />
         <InputField
-          label="Phone number"
+          // label="Phone number"
           type="text"
           name="contact_phone"
-          placeholder="Enter phone number"
+          placeholder="Phone"
           value={phone}
           onChange={setPhone}
           required={true}
         />
       </div>
       <button
-        className="rounded-md bg-primary px-5 py-2 text-text hover:bg-primary-200 hover:text-text flex items-center justify-center min-w-[120px]"
+        className="flex w-full items-center justify-center gap-2 self-start rounded-md bg-primary px-5 py-2 font-semibold tracking-wide text-text transition-colors hover:bg-primary-200 hover:text-text"
         type="submit"
-        aria-label="Submit form and close overlay."
+        aria-label="Submit form."
       >
-        <span className={loading ? 'mr-2' : ''}>Register</span>
-        {loading && <MotionSpinner/>}
+        <span>Register</span>
+        {loading && <MotionSpinner />}
       </button>
     </form>
   );
