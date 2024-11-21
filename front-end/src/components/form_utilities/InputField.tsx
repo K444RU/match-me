a33a1a304ce25e27d5d49f7ef77127ed1520e799
Karl Romet Somelar <karlromets@gmail.com>
2024-11-21 13:45:48 +0200

    refactor: modify InputField removed unneccesary label field as it shows the same as the placeholder anyway refactored the styling to be more universal and clean removed the useless double parent div (now 1 parent which you can pass classes into.)

--- front-end/src/components/form_utilities/InputField.tsx
+++ front-end/src/components/form_utilities/InputField.tsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React from 'react';
 
 type InputFieldProps = {
-  label: string;
+  // label: string;
   name: string; // [htmlFor, id, name]
   type: string;
   placeholder?: string;
@@ -9,27 +9,36 @@ type InputFieldProps = {
   onChange: React.Dispatch<React.SetStateAction<string>>;
   required?: boolean;
   className?: string;
-}
+};
 
 const InputField = (
-  { label, name, type, placeholder, value, onChange, required, className}: InputFieldProps) => {
+  // { label, name, type, placeholder, value, onChange, required, className}: InputFieldProps) => {
+  {
+    name,
+    type,
+    placeholder,
+    value,
+    onChange,
+    required,
+    className,
+  }: InputFieldProps
+) => {
   return (
-    <div className={`mb-3 place-items-start ${className}`}>
-      <label className="ml-1" htmlFor={name}>{label}</label>
-      <div className="rounded-md border border-primary bg-text-100 p-1">
+    <div className={`w-full ${className ? className : ''}`}>
+      {/* <label className="ml-1" htmlFor={`${name}`}>{label}</label> */}
+      {/* file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground*/}
         <input
           type={type}
           id={name}
           name={name}
-          className="bg-text-100 w-full"
+          className="focus-visible:ring-ring flex h-10 w-full rounded-md border border-accent bg-primary-50 px-3 py-2 text-base ring-accent-500 placeholder:text-text-300 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm outline-0"
           placeholder={placeholder}
           required={required}
           value={value}
           onChange={(e) => onChange(e.target.value)}
         />
-      </div>
     </div>
-  )
-}
+  );
+};
 
-export default InputField
+export default InputField;
