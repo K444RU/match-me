e303c01902f1e39b798c6edde223a503db59e5a6
Karl Romet Somelar <karlromets@gmail.com>
2024-11-21 14:09:01 +0200

    feat: add dynamic FormResponse state if error > red, if success > green, default error.

--- front-end/src/components/form_utilities/FormResponse.tsx
+++ front-end/src/components/form_utilities/FormResponse.tsx
@@ -1,12 +1,20 @@
 const FormResponse = ({
   title = 'Something went wrong...',
   subtitle = 'Please contact support.',
+  state = 'error',
 }: {
   title?: string;
   subtitle?: string;
+  state?: 'error' | 'success';
 }) => {
   return (
-    <div className="flex w-full flex-col items-center justify-center border border-red-800 bg-red-200 p-2">
+    <div
+      className={`${
+        state === 'error'
+          ? 'border-red-800 bg-red-200'
+          : 'border-green-800 bg-green-200'
+      } flex w-full flex-col items-center justify-center border p-2`}
+    >
       <h2 className="text-base font-semibold md:text-sm">{title}</h2>
       <p className="text-sm text-text-500 md:text-xs">{subtitle}</p>
     </div>
