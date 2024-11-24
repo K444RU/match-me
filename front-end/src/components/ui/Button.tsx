cb3573130d4e6bfae855c4d5da8f3fda8ace9901
Karl Romet Somelar <karlromets@gmail.com>
2024-11-24 20:52:44 +0200

    feat: add button component

---
+++ front-end/src/components/ui/Button.tsx
@@ -0,0 +1,13 @@
+import { forwardRef } from "react";
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  onClick?: () => void;
+  children: React.ReactNode;
+}
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(({ onClick, children }, ref) => {
+  return (
+    <button ref={ref} type="button" onClick={onClick} className='focus-visible:ring-ring text-text inline-flex h-10 items-center justify-center gap-2 whitespace-nowrap rounded-md bg-primary px-4 py-2 text-sm font-semibold tracking-wide ring-offset-background transition-colors duration-300 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0'>{children}</button>
+  )
+})
+
+export default Button
