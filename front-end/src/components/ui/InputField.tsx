b656fd36fb3abb4a5e8d573db34f39e1a13d65a9
Karl Romet Somelar <karlromets@gmail.com>
2024-11-24 15:53:31 +0200

    style: modify /register & /login

--- front-end/src/components/ui/InputField.tsx
+++ front-end/src/components/ui/InputField.tsx
@@ -24,14 +24,14 @@ const InputField = (
   }: InputFieldProps
 ) => {
   return (
-    <div className={`mb-3 ${className ? className : ''}`}>
+    <div className={className ? className : 'w-full'}>
       {/* <label className="ml-1" htmlFor={`${name}`}>{label}</label> */}
       {/* file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground*/}
       <input
         type={type}
         id={name}
         name={name}
-        className="focus-visible:ring-ring flex h-10 w-full rounded-md border border-accent bg-primary-50 px-3 py-2 text-base outline-0 ring-accent-500 placeholder:text-text-300 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm"
+        className="focus-visible:ring-ring flex h-10 w-full rounded-md border border-accent bg-primary-50 px-3 py-2 text-base outline-0 ring-accent-500 placeholder:text-text-300 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm ring-offset-accent-200"
         placeholder={placeholder}
         required={required}
         value={value}
--- front-end/src/features/authentication/components/LoginPage.tsx
+++ front-end/src/features/authentication/components/LoginPage.tsx
@@ -18,7 +18,7 @@ const LoginPage = () => {
 
   return (
     <div className="flex h-screen items-center justify-center bg-background">
-      <div className="w-full max-w-md p-6">
+      <div className="w-full max-w-md rounded-md bg-accent-200 p-6">
         <h1 className="mb-6 text-center text-3xl font-bold text-text">Login</h1>
         <LoginForm/>
       </div>
