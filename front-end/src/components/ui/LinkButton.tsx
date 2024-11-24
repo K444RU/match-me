47b09e0bc7453e86c2e28187e21ac7a8ba33024c
Karl Romet Somelar <karlromets@gmail.com>
2024-11-24 15:31:59 +0200

    style: linkbutton gesture effex

--- front-end/src/components/ui/LinkButton.tsx
+++ front-end/src/components/ui/LinkButton.tsx
@@ -1,16 +1,26 @@
 import { ReactNode } from 'react';
 import { Link } from 'react-router-dom';
+import { motion } from 'motion/react';
+
+const MotionLink = motion(Link);
 
 interface LinkButtonProps {
-    to: string;
-    content : ReactNode
+  to: string;
+  content: ReactNode;
 }
 
-const LinkButton = ({to, content}: LinkButtonProps) => {
+const LinkButton = ({ to, content }: LinkButtonProps) => {
   return (
-    <Link to={to} className="focus-visible:ring-ring text-primary-foreground hover:bg-primary/90 inline-flex h-10 items-center justify-center gap-2 whitespace-nowrap rounded-md bg-primary-50 px-4 py-2 text-sm font-semibold tracking-wide ring-offset-background transition-colors duration-300 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0">
+    <MotionLink
+      to={to}
+      className="focus-visible:ring-ring text-primary-foreground hover:bg-primary/90 inline-flex h-10 items-center justify-center gap-2 whitespace-nowrap rounded-md bg-primary-50 px-4 py-2 text-sm font-semibold tracking-wide ring-offset-background transition-colors duration-300 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0"
+      whileHover={{
+        scale: 1.1,
+      }}
+      whileTap={{ scale: 0.9 }}
+    >
       {content}
-    </Link>
+    </MotionLink>
   );
 };
 
