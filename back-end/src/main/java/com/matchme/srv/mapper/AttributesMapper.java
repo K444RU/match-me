4ec3961024aeca62319371167ef77439063d634a
Karl Romet Somelar <karlromets@gmail.com>
2024-12-05 19:04:55 +0200

    fix: mvn crash on startup
    - location if a list of doubles
    - getLongitude doesn't work for records because records don't use get convetion, just use the variable name so .longitude() returns the value
    - added required list import

--- back-end/src/main/java/com/matchme/srv/mapper/AttributesMapper.java
+++ back-end/src/main/java/com/matchme/srv/mapper/AttributesMapper.java
@@ -2,11 +2,17 @@ package com.matchme.srv.mapper;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import java.util.List;
 
 import com.matchme.srv.dto.request.UserParametersRequestDTO;
 import com.matchme.srv.model.user.profile.user_attributes.UserAttributes;
 
-@Mapper(componentModel = "spring")
+@Mapper(
+    componentModel = "spring",
+    imports = {
+        java.util.List.class
+    }
+)
 public interface AttributesMapper {
   
   @Mapping(target = "id", ignore = true)
@@ -14,6 +20,6 @@ public interface AttributesMapper {
   @Mapping(target = "attributeChangeLog", ignore = true)
   @Mapping(source = "gender_self", target = "gender")
   @Mapping(source = "birthDate", target = "birthDate")
-  @Mapping(target = "location", expression = "java(location.add(parameters.getLongitude(), parameters.getLatitude())")
+  @Mapping(target = "location", expression = "java(List.of(parameters.longitude(), parameters.latitude()))")
   UserAttributes toEntity(UserParametersRequestDTO parameters);
 }
