b1c4ab204743a618f303aa63bca32e1722687e2a
Karl Romet Somelar <karlromets@gmail.com>
2024-12-14 22:29:36 +0200

    refactor: UserInfo useAuth

--- front-end/src/pages/chats/components/UserInfo.tsx
+++ front-end/src/pages/chats/components/UserInfo.tsx
@@ -1,33 +1,16 @@
 import { IoSettingsOutline } from 'react-icons/io5';
 import { FaRegUserCircle } from 'react-icons/fa';
-import { useEffect, useState } from 'react';
-import { CurrentUser } from '@/types/api.ts';
-import { getCurrentUser } from '@/pages/UserService.ts';
 import { Skeleton } from '@/components/ui/Skeleton';
+import { useAuth } from '@/features/authentication/AuthContext';
 
 const UserInfo = () => {
-    const [currentUser, setCurrentUser] = useState<CurrentUser | null>(null);
-    const [loading, setLoading] = useState<boolean>(true);
-    const [error, setError] = useState<string | null>(null);
+    // Dunno how to see loading state since we are not awaiting useAuth...
+    // const [loading, setLoading] = useState<boolean>(true);
+    // const [error, setError] = useState<string | null>(null);
+    // Maybe we don't need it anyway
 
-    useEffect(() => {
-        const fetchCurrentUser = async () => {
-            try {
-                const data = await getCurrentUser();
-                console.log('✅ User profile fetched successfully:', data);
-                setCurrentUser(data);
-            } catch (err) {
-                console.error('❌ Error fetching user profile:', err);
-                setError('Failed to load user profile.');
-            } finally {
-                setLoading(false);
-            }
-        };
-
-        fetchCurrentUser();
-    }, []);
-
-    if (error) return <div>{error}</div>;
+    const {user} = useAuth();
+    if(!user) return;
 
     return (
         <div className="relative mt-20 flex h-40 w-full items-center bg-primary-200 text-text">
@@ -35,12 +18,12 @@ const UserInfo = () => {
             <FaRegUserCircle className="ml-2 mr-2 h-20 w-20 text-text-600" />
             <div>
                 <h2 className="ml-2 text-2xl font-bold text-text-700">
-                    {currentUser?.firstName || (
+                    {user?.firstName || (
                         <Skeleton className="h-[32px] w-[120px] bg-text-300 rounded-md" />
                     )}
                 </h2>
                 <h2 className="ml-2 text-lg font-bold text-text-700">
-                    {currentUser?.alias || (
+                    {user?.alias || (
                         <Skeleton className="mt-1 h-[28px] w-[100px] bg-text-300 rounded-md" />
                     )}
                 </h2>
