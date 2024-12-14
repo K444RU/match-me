be60e2fd696e41be770fe02ed58e078aac09fe6e
Karl Romet Somelar <karlromets@gmail.com>
2024-12-15 00:04:18 +0200

    fix: auth layout bugs

--- front-end/src/router/router.tsx
+++ front-end/src/router/router.tsx
@@ -13,7 +13,7 @@ import { useEffect } from 'react';
 import LoginPage from '@/features/authentication/components/LoginPage';
 import RegisterPage from '@/features/authentication/components/RegisterPage';
 import ProfileCompletionPage from '@/pages/profile-completion/ProfileCompletionPage';
-import AuthenticatedLayout from '@/layout/AuthenticatedLayout';
+import AuthenticatedLayout from '@/components/layout/AuthenticatedLayout';
 
 const LogoutPage = () => {
     const { user, logout } = useAuth();
@@ -27,25 +27,31 @@ const LogoutPage = () => {
 export const routes = createRoutesFromElements(
     <Route>
         {/* Public routes with main layout */}
-        <Route element={<MainLayout/>}>
-            <Route index element={<HomePage/>}/>
+        <Route element={<MainLayout />}>
+            <Route index element={<HomePage />} />
 
             {/* Login page in case unauthenticated */}
-            <Route element={<AuthenticationGuard guardType="unauthenticated"/>}>
-                <Route path="login" element={<LoginPage/>}/>
-                <Route path="register" element={<RegisterPage/>}/>
+            <Route
+                element={<AuthenticationGuard guardType="unauthenticated" />}
+            >
+                <Route path="login" element={<LoginPage />} />
+                <Route path="register" element={<RegisterPage />} />
             </Route>
         </Route>
 
-            {/* Protected Routes */}
+        {/* Protected Routes */}
+        {/* Auth guard before layout to prevent unnecessary rendering */}
+        <Route element={<AuthenticationGuard />}>
             <Route element={<AuthenticatedLayout />}>
-                <Route element={<AuthenticationGuard/>}>
-                    <Route path="settings" element={<SettingsPage/>}/>
-                    <Route path="profile-completion" element={<ProfileCompletionPage/>}/>
-                    <Route path="chats" element={<ChatsPage/>}/>
-                    <Route path="logout" element={<LogoutPage/>}/>
-                </Route>
+                <Route path="settings" element={<SettingsPage />} />
+                <Route
+                    path="profile-completion"
+                    element={<ProfileCompletionPage />}
+                />
+                <Route path="chats" element={<ChatsPage />} />
+                <Route path="logout" element={<LogoutPage />} />
             </Route>
+        </Route>
     </Route>
 );
 
