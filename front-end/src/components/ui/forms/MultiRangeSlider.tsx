f10c31e2479b4f9dc2c5f9ceb2c7b84f8085a0f0
Karl Romet Somelar <karlromets@gmail.com>
2024-12-17 15:02:12 +0200

    style: skeletons

--- front-end/src/components/ui/forms/MultiRangeSlider.tsx
+++ front-end/src/components/ui/forms/MultiRangeSlider.tsx
@@ -1,4 +1,5 @@
 import MultiRangeSlider, { ChangeResult } from 'multi-range-slider-react';
+import { Skeleton } from '../skeleton';
 
 type MultiRangeSliderProps = {
     min: number;
@@ -13,8 +14,8 @@ type MultiRangeSliderProps = {
 const MultiHandleSlider = ({
     min,
     max,
-    minValue=18,
-    maxValue=120,
+    minValue,
+    maxValue,
     onInput,
     label,
     showInputField = true,
@@ -24,21 +25,27 @@ const MultiHandleSlider = ({
             {label && <h2 className="pl-1 font-semibold">{label}</h2>}
             <div className="flex items-center gap-4">
                 <div className="w-full">
-                    <MultiRangeSlider
-                        min={min}
-                        max={max}
-                        minValue={minValue ?? 18}
-                        maxValue={maxValue ?? 120}
-                        step={1}
-                        onInput={onInput}
-                        style={{ boxShadow: 'none' }}
-                        ruler={false}
-                        barLeftColor="white"
-                        barInnerColor="#e67d60"
-                        barRightColor="white"
-                        thumbLeftColor="#e67d60"
-                        thumbRightColor="#e67d60"
-                    />
+                    {minValue !== null && maxValue !== null ? (
+                        <MultiRangeSlider
+                            min={min}
+                            max={max}
+                            minValue={minValue}
+                            maxValue={maxValue}
+                            step={1}
+                            onInput={onInput}
+                            style={{ boxShadow: 'none' }}
+                            ruler={false}
+                            barLeftColor="white"
+                            barInnerColor="#e67d60"
+                            barRightColor="white"
+                            thumbLeftColor="#e67d60"
+                            thumbRightColor="#e67d60"
+                        />
+                    ) : (
+                        <div className="w-full">
+                            <Skeleton className="h-[58.8px] w-full rounded-[10px] border border-[gray]" />
+                        </div>
+                    )}
                 </div>
                 {showInputField && (
                     <div className="flex items-center gap-4">
@@ -46,26 +53,34 @@ const MultiHandleSlider = ({
                             <label htmlFor="minValue" className="sr-only">
                                 Min Value
                             </label>
-                            <input
-                                id="minValue"
-                                type="number"
-                                value={minValue ?? 18}
-                                className="focus-visible:ring-ring flex h-10 w-16 rounded-md border border-accent bg-primary-50 px-3 py-2 text-base outline-0 ring-accent-500 placeholder:text-text-300 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm"
-                                readOnly
-                            />
+                            {minValue !== null && maxValue !== null ? (
+                                <input
+                                    id="minValue"
+                                    type="number"
+                                    value={minValue}
+                                    className="focus-visible:ring-ring flex h-10 w-16 rounded-md border border-accent bg-primary-50 px-3 py-2 text-base outline-0 ring-accent-500 placeholder:text-text-300 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm"
+                                    readOnly
+                                />
+                            ) : (
+                                <span>loading</span>
+                            )}
                         </div>
                         <span>to</span>
                         <div>
                             <label htmlFor="maxValue" className="sr-only">
                                 Max Value
                             </label>
-                            <input
-                                id="maxValue"
-                                type="number"
-                                value={maxValue ?? 120}
-                                className="focus-visible:ring-ring flex h-10 w-16 rounded-md border border-accent bg-primary-50 px-3 py-2 text-base outline-0 ring-accent-500 placeholder:text-text-300 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm"
-                                readOnly
-                            />
+                            {minValue !== null && maxValue !== null ? (
+                                <input
+                                    id="maxValue"
+                                    type="number"
+                                    value={maxValue}
+                                    className="focus-visible:ring-ring flex h-10 w-16 rounded-md border border-accent bg-primary-50 px-3 py-2 text-base outline-0 ring-accent-500 placeholder:text-text-300 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm"
+                                    readOnly
+                                />
+                            ) : (
+                                <span>loading</span>
+                            )}
                         </div>
                     </div>
                 )}
