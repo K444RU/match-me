bb3588c226386ff5405e109f027f1d01fe6810bf
Karl Romet Somelar <karlromets@gmail.com>
2024-12-17 09:54:57 +0200

    feat: add 404

---
+++ front-end/src/pages/404Page.tsx
@@ -0,0 +1,10 @@
+const PageNotFound = () => {
+    return (
+        <div className="h-screen text-4xl tracking-tight dark:text-white sm:text-5xl lg:text-6xl space-x-4 flex flex-col justify-center w-full text-center items-center">
+        <span className="font-extrabold">404</span>
+        <span className="">eksisime ära...</span>
+      </div>
+    );
+};
+
+export default PageNotFound;
