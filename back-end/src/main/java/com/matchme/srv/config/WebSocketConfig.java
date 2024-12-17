43887b221207f4a35171a6bd64fe12a289bf5435
andreasholst <andreasholst1997@gmail.com>
2024-12-17 13:31:48 +0200

    fix: add csrf token

--- back-end/src/main/java/com/matchme/srv/config/WebSocketConfig.java
+++ back-end/src/main/java/com/matchme/srv/config/WebSocketConfig.java
@@ -1,12 +1,19 @@
 package com.matchme.srv.config;
 
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.handler.WebSocketHandlerDecorator;
+import org.springframework.web.socket.handler.WebSocketHandlerDecoratorFactory;
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -30,6 +37,25 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
   public void registerStompEndpoints(StompEndpointRegistry registry) {
     registry.addEndpoint("stomp") //connects to the configured endpoint here
       .setAllowedOrigins("*")
-      .withSockJS(); // enable SockJS fallback;
+      .withSockJS() // enable SockJS fallback;
+      .setSessionCookieNeeded(true);
+  }
+
+  // TODO: Is this necessary? 
+  @Bean
+  public WebSocketHandlerDecoratorFactory wsHandlerDecoratorFactory() {
+    return new WebSocketHandlerDecoratorFactory() {
+      @Override
+      public WebSocketHandler decorate(WebSocketHandler handler) {
+        return new WebSocketHandlerDecorator(handler) {
+          @Override
+          public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+            String csrfToken = UUID.randomUUID().toString();
+            session.getAttributes().put("CSRF_TOKEN", csrfToken);
+            super.afterConnectionEstablished(session);
+          }
+        };
+      }
+    };
   }
 }
