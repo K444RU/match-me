bb85488b7c033b0a9378e4ec0696eb01f1fbb3c6
Karl Romet Somelar <karlromets@gmail.com>
2024-12-17 15:02:12 +0200

    style: skeletons

--- front-end/src/components/ui/forms/OneHandleSlider.tsx
+++ front-end/src/components/ui/forms/OneHandleSlider.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { Skeleton } from '../skeleton';
 
 type OneHandleSliderProps = {
     name: string;
@@ -17,48 +17,48 @@ const OneHandleSlider = ({
     min,
     max,
     step,
-    value=50,
+    value,
     onChange,
     label,
     className,
     showInputField = true,
 }: OneHandleSliderProps) => {
-    const [displayNumber, setDisplayNumber] = useState<number>(value ?? min);
-
-    useEffect(() => {
-        setDisplayNumber(value ?? min);
-    }, [value]);
 
     const handleSliderMovement = (value: number) => {
-        setDisplayNumber(value ?? min);
-        onChange(value ?? min);
+        onChange(value);
     };
 
     return (
         <div className={`mb-3 w-full text-text ${className ? className : ''}`}>
             <h2 className="pl-1 font-semibold">{label}</h2>
             <div className="flex items-center gap-4">
-                <input
-                    className="h-4 w-1/2 cursor-pointer appearance-none rounded-3xl border border-accent bg-primary-50"
-                    type="range"
-                    id={name}
-                    name={name}
-                    min={min}
-                    max={max}
-                    value={displayNumber}
-                    step={step}
-                    onChange={(e) =>
-                        handleSliderMovement(Number(e.target.value))
-                    }
-                />
-                {showInputField && (
+                {value !== null ? (
+                    <input
+                        className="h-4 w-1/2 cursor-pointer appearance-none rounded-3xl border border-accent bg-primary-50"
+                        type="range"
+                        id={name}
+                        name={name}
+                        min={min}
+                        max={max}
+                        value={value}
+                        step={step}
+                        onChange={(e) =>
+                            handleSliderMovement(Number(e.target.value))
+                        }
+                    />
+                ) : (
+                    <div className="w-1/2">
+                        <Skeleton className="h-4 w-full rounded-3xl border border-accent" />
+                    </div>
+                )}
+                {showInputField && value !== null && (
                     <div className="flex flex-wrap items-center gap-1">
                         <input
                             className="focus-visible:ring-ring flex h-10 w-16 rounded-md border border-accent bg-primary-50 px-3 py-2 text-base outline-0 ring-accent-500 placeholder:text-text-300 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm"
                             type="text"
                             id="display_value"
                             name="display_value"
-                            value={displayNumber}
+                            value={value}
                             onChange={(e) =>
                                 handleSliderMovement(Number(e.target.value))
                             }
