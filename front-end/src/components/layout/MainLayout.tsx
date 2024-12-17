b6bbf92c6ce6b083c03b949727f72ee2f3882366
Karl Romet Somelar <karlromets@gmail.com>
2024-12-17 10:16:35 +0200

    feat: add ThemeProvider

--- front-end/src/components/layout/MainLayout.tsx
+++ front-end/src/components/layout/MainLayout.tsx
@@ -1,12 +1,13 @@
 import { Outlet } from 'react-router-dom';
 import Navbar from '../Navbar';
+import { ThemeProvider } from '../ThemeProvider';
 
 const MainLayout = () => {
     return (
-        <>
+        <ThemeProvider defaultTheme='light' storageKey="vite-ui-theme">
             <Navbar />
             <Outlet />
-        </>
+        </ThemeProvider>
     );
 };
 
