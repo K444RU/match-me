3e053dce8940b6a4039f0fb0aac57b6da6b46229
Karl Romet Somelar <karlromets@gmail.com>
2024-12-17 10:16:35 +0200

    feat: add ThemeProvider

---
+++ front-end/src/components/ThemeToggler.tsx
@@ -0,0 +1,36 @@
+
+import { Moon, Sun } from "lucide-react";
+import { useTheme } from "./ThemeProvider";
+import { Button } from "./ui/button";
+import { useEffect, useState } from "react";
+
+const ThemeToggler = () => {
+  const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return (
+      <Button variant="ghost" size="icon">
+        <span className="h-4 w-4" />
+      </Button>
+    );
+  }
+
+  return (
+    <Button
+      variant="ghost"
+      size="icon"
+      onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+      title={`Switch to ${theme === "dark" ? "light" : "dark"} theme`}
+    >
+      {theme === "dark" ? <Sun /> : <Moon />}
+      <span className="sr-only">Toggle theme</span>
+    </Button>
+  );
+};
+
+export default ThemeToggler;
