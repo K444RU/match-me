31db7acdc2e3c58747470afe437ac717cb3abd92
Karl Romet Somelar <karlromets@gmail.com>
2024-12-17 09:54:57 +0200

    feat: add 404

--- front-end/src/router/router.tsx
+++ front-end/src/router/router.tsx
@@ -14,6 +14,7 @@ import LoginPage from '@/features/authentication/components/LoginPage';
 import RegisterPage from '@/features/authentication/components/RegisterPage';
 import ProfileCompletionPage from '@/pages/profile-completion/ProfileCompletionPage';
 import AuthenticatedLayout from '@/components/layout/AuthenticatedLayout';
+import PageNotFound from '@/pages/404Page';
 
 const LogoutPage = () => {
     const { user, logout } = useAuth();
@@ -37,6 +38,10 @@ export const routes = createRoutesFromElements(
                 <Route path="login" element={<LoginPage />} />
                 <Route path="register" element={<RegisterPage />} />
             </Route>
+            <Route
+                    path="*"
+                    element={<PageNotFound />}
+                />
         </Route>
 
         {/* Protected Routes */}
