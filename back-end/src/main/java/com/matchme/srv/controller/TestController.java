cf52ec9dab0b0061fd8ea9a03b3592e2a70ca46a
Karl Romet Somelar <karlromets@gmail.com>
2024-12-19 18:40:21 +0200

    feat: back-end endpoints for bulk test data insertion

--- back-end/src/main/java/com/matchme/srv/controller/TestController.java
+++ back-end/src/main/java/com/matchme/srv/controller/TestController.java
@@ -1,15 +1,51 @@
 package com.matchme.srv.controller;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.request;
+
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.matchme.srv.dto.request.MessagesSendRequestDTO;
+import com.matchme.srv.dto.request.SignupRequestDTO;
+import com.matchme.srv.dto.request.UserParametersRequestDTO;
+import com.matchme.srv.dto.response.ChatMessageResponseDTO;
+import com.matchme.srv.dto.response.ConnectionResponseDTO;
+import com.matchme.srv.model.connection.Connection;
+import com.matchme.srv.model.user.User;
+import com.matchme.srv.service.ChatService;
+import com.matchme.srv.service.ConnectionService;
+import com.matchme.srv.service.UserService;
+
+import jakarta.validation.Valid;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api/test")
 public class TestController {
+
+  @Autowired
+  UserService userService;
+
+  @Autowired
+  ConnectionService connectionService;
+
+  @Autowired
+  ChatService chatService;
+
   @GetMapping("/all")
   public String allAccess() {
     return "Public Content.";
@@ -32,4 +68,94 @@ public class TestController {
   public String adminAccess() {
     return "Admin Board.";
   }
+
+  @PostMapping("/users")
+  public ResponseEntity<?> bulkCreateUsers(@Valid @RequestBody List<SignupRequestDTO> users) {
+    int batchSize = 50;
+
+    for (int i = 0; i < users.size(); i += batchSize) {
+      int endIndex = Math.min(i + batchSize, users.size());
+      List<SignupRequestDTO> batch = users.subList(i, endIndex);
+
+      for (SignupRequestDTO user : batch) {
+        userService.createUser(user);
+      }
+    }
+    return ResponseEntity.status(HttpStatus.CREATED).build();
+  }
+
+  @PostMapping("/users/finish")
+  public ResponseEntity<?> bulkSetUserParameters(@Valid @RequestBody UserParametersWithEmailsDTO request) {
+    List<UserParametersRequestDTO> users = request.parameters();
+    List<String> emails = request.emails();
+
+    for (int i = 0; i < users.size(); i++) {
+      User existingUser = userService.getUserByEmail(emails.get(i));
+      userService.setUserParameters(existingUser.getId(), users.get(i));
+    }
+    return ResponseEntity.status(HttpStatus.CREATED).build();
+  }
+
+  @PostMapping("/connections")
+  public ResponseEntity<List<ConnectionResponseDTO>> bulkCreateConnections(@Valid @RequestBody List<UserPair> request) {
+    List<ConnectionResponseDTO> createdConnections = new ArrayList<>();
+    int batchSize = 100;
+
+    for (int i = 0; i < request.size(); i += batchSize) {
+      int endIndex = Math.min(i + batchSize, request.size());
+      List<UserPair> batch = request.subList(i, endIndex);
+
+      for (UserPair pair : batch) {
+        User user1 = userService.getUserByEmail(pair.email1());
+        User user2 = userService.getUserByEmail(pair.email2());
+        createdConnections.add(connectionService.createConnection(user1, user2));
+      }
+    }
+    return ResponseEntity.status(HttpStatus.CREATED).body(createdConnections);
+  }
+
+  @PostMapping("/messages")
+  public ResponseEntity<List<ChatMessageResponseDTO>> bulkCreateMessages(
+      @Valid @RequestBody List<MessagesSendRequestDTOWithSender> request) {
+    List<ChatMessageResponseDTO> createdChatMessages = new ArrayList<>();
+    int batchSize = 100;
+
+    for (int i = 0; i < request.size(); i += batchSize) {
+      int endIndex = Math.min(i + batchSize, request.size());
+      List<MessagesSendRequestDTOWithSender> batch = request.subList(i, endIndex);
+
+      for (MessagesSendRequestDTOWithSender message : batch) {
+        User sender = userService.getUserByEmail(message.senderEmail());
+        createdChatMessages.add(chatService.saveMessage(
+            message.connectionId(),
+            sender.getId(),
+            message.content(),
+            message.timestamp()));
+      }
+    }
+    return ResponseEntity.status(HttpStatus.CREATED).body(createdChatMessages);
+  }
+
+  @DeleteMapping("/users")
+  public ResponseEntity<?> bulkDeleteUsersByEmail(@RequestBody List<String> emails) {
+    for (int i = 0; i < emails.size(); i++) {
+      userService.removeUserByEmail(emails.get(i));
+    }
+    return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+  }
+
+  public record UserParametersWithEmailsDTO(
+      List<UserParametersRequestDTO> parameters,
+      List<String> emails) {
+  }
+
+  public record UserPair(String email1, String email2) {
+  };
+
+  public record MessagesSendRequestDTOWithSender(
+    Long connectionId,
+    String content,
+    String senderEmail,
+    Timestamp timestamp
+) {}
 }
