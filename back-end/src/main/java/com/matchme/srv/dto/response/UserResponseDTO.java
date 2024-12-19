e2ddbeb4660945115c1123fea770f7175347ecbc
Karl Romet Somelar <karlromets@gmail.com>
2024-12-19 18:42:30 +0200

    fix: simpler entity response DTOs
    - because of all relations back-end kept throwing errors related to relations and other stuff
    
    - created UserReponseDTO and ConnectionResponseDTO for simpler responses instead of returning full entity

--- back-end/src/main/java/com/matchme/srv/dto/response/UserResponseDTO.java
+++ back-end/src/main/java/com/matchme/srv/dto/response/UserResponseDTO.java
@@ -1,7 +1,36 @@
 package com.matchme.srv.dto.response;
 
-public record UserResponseDTO(
-  String email, 
+import com.matchme.srv.model.user.UserRoleType;
+import com.matchme.srv.model.user.UserStateTypes;
+import com.matchme.srv.model.user.activity.ActivityLog;
+import com.matchme.srv.model.user.profile.UserProfile;
+import com.matchme.srv.model.user.profile.user_score.UserScore;
 
-  String number 
+/**
+* Custom DTO so we don't return UserAuth
+*
+ * @param id
+ * @param email
+ * @param number
+ * @param state - current state of the user account
+ * @param roles - set of roles assigned to the user
+ * @param activity - set of user activity logs
+ * @param profile
+ * @param score
+* @return UserResponseDTO
+* @see  UserStateTypes
+* @see  UserRoleType
+* @see  ActivityLog
+* @see  UserProfile
+* @see  UserScore
+*/
+public record UserResponseDTO(
+  Long id,
+  String email,
+  String number
+  // UserStateTypes state,
+  // Set<UserRoleType> roles,
+  // Set<ActivityLog> activity,
+  // UserProfile profile,
+  // UserScore score
 ) {}
