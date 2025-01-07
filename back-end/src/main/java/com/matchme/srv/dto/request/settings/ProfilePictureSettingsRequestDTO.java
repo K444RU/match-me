72e6e990729511198cbce3508d664d3db51e3439
olegtrofimov <olegtrofimov96@gmail.com>
2025-01-07 19:59:33 +0200

    forgotten uncommit changes

--- back-end/src/main/java/com/matchme/srv/dto/request/settings/ProfilePictureSettingsRequestDTO.java
+++ back-end/src/main/java/com/matchme/srv/dto/request/settings/ProfilePictureSettingsRequestDTO.java
@@ -1,10 +1,14 @@
 package com.matchme.srv.dto.request.settings;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProfilePictureSettingsRequestDTO {
     private String base64Image;
 }
