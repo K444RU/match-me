231b8154bec897fefa4a6bf49e8f684b80e5cde2
Karl Romet Somelar <karlromets@gmail.com>
2025-01-09 19:51:36 +0200

    chore: fix imports

--- front-end/src/features/authentication/index.ts
+++ front-end/src/features/authentication/index.ts
@@ -3,3 +3,4 @@ export { default as RegisterPage } from './components/RegisterPage';
 export {default as Login } from './components/PopUpForm'
 export { useAuth } from './AuthContext';
 export { authService } from './services/auth-service';
+export { AuthProvider } from './auth-provider'
