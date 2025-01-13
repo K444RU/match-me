b05f9ba73f0848d0968757a1acbe21e18bd0fa5c
andreasholst <andreasholst1997@gmail.com>
2025-01-13 13:06:02 +0200

    feat: add ChatProvider to authenticated layout

--- front-end/src/components/layout/AuthenticatedLayout.tsx
+++ front-end/src/components/layout/AuthenticatedLayout.tsx
@@ -1,15 +1,18 @@
 import { FC } from 'react';
 import { SidebarProvider } from '../ui/sidebar';
 import { Outlet } from 'react-router-dom';
+import { ChatProvider } from '@pages/chats/ChatContext';
 
 // Syntax recommended by Claude after asking about props - kinda cool
 const AuthenticatedLayout: FC = () => {
     return (
-        <SidebarProvider>
-            <div className="flex h-screen w-screen overflow-hidden">
-                <Outlet />
-            </div>
-        </SidebarProvider>
+        <ChatProvider>
+            <SidebarProvider>
+                <div className="flex h-screen w-screen overflow-hidden">
+                    <Outlet />
+                </div>
+            </SidebarProvider>
+        </ChatProvider>
     );
 };
 
