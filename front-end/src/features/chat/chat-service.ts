51c137104dbbf069ec5d0693dd52f78b1ddad942
Karl Romet Somelar <karlromets@gmail.com>
2025-01-13 11:44:24 +0200

    refactor: move chat stuff to features folder
    - Also rename ChatService to kebab-case

---
+++ front-end/src/features/chat/chat-service.ts
@@ -0,0 +1,10 @@
+const sendMessage = (message: string, to: number, from: string) => {
+  // to = connectionId
+  // from = authToken which will get turned into userId in backend
+  // We don't want to let the user pick sender himself.
+  console.log(message);
+  console.log(to);
+  console.log(from);
+};
+
+export default sendMessage;
