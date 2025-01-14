8cf12020b186ddb0082bdaacff1ea60fdf9e731c
Karl Romet Somelar <karlromets@gmail.com>
2025-01-14 12:07:51 +0200

    fix(settings): add await to refreshSettings calls

--- front-end/src/pages/user-settings/components/UserAttributesCard.tsx
+++ front-end/src/pages/user-settings/components/UserAttributesCard.tsx
@@ -1,189 +1,163 @@
-import { useContext, useEffect, useState } from 'react';
-import { Button } from '@/components/ui/button';
-import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardFooter,
-    CardHeader,
-    CardTitle,
-} from '@/components/ui/card';
-import { Label } from '@/components/ui/label';
-import {
-    Select,
-    SelectContent,
-    SelectItem,
-    SelectTrigger,
-    SelectValue,
-} from '@/components/ui/select';
-import { SettingsContext } from '../SettingsContext';
-import DatePicker from '@/components/ui/forms/DatePicker';
-import { GenderContext } from '@/features/gender';
-import { CitySuggestions } from '@/pages/profile-completion/components/CitySuggestions';
-import { useDebounce } from '@/lib/hooks/useDebounce';
-import { City } from '@/pages/profile-completion/types/types';
-import InputField from '@/components/ui/forms/InputField';
-import { toast } from 'sonner';
-import { userService } from '@/features/user';
 import MotionSpinner from '@/components/animations/MotionSpinner';
+import { Button } from '@/components/ui/button';
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
+import DatePicker from '@/components/ui/forms/DatePicker';
+import InputField from '@/components/ui/forms/InputField';
+import { Label } from '@/components/ui/label';
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
 import { Skeleton } from '@/components/ui/skeleton';
+import { GenderContext } from '@/features/gender';
+import { userService } from '@/features/user';
+import { useDebounce } from '@/lib/hooks/useDebounce';
+import { CitySuggestions } from '@/pages/profile-completion/components/CitySuggestions';
+import { City } from '@/pages/profile-completion/types/types';
+import { useContext, useEffect, useState } from 'react';
+import { toast } from 'sonner';
+import { SettingsContext } from '../SettingsContext';
 
 const UserAttributesCard = () => {
-    const genders = useContext(GenderContext);
-    const settingsContext = useContext(SettingsContext);
-    const [city, setCity] = useState<string>();
-    const [longitude, setLongitude] = useState<number | null>(null);
-    const [latitude, setLatitude] = useState<number | null>(null);
-    const [gender, setGender] = useState<number | null>(null);
-    const [birthDate, setBirthDate] = useState<Date | undefined>(undefined);
-    const [showSuggestions, setShowSuggestions] = useState(false);
-    const debouncedCitySearchValue = useDebounce(city as string, 1000);
-    const [loading, setLoading] = useState(false);
+  const genders = useContext(GenderContext);
+  const settingsContext = useContext(SettingsContext);
+  const [city, setCity] = useState<string>();
+  const [longitude, setLongitude] = useState<number | null>(null);
+  const [latitude, setLatitude] = useState<number | null>(null);
+  const [gender, setGender] = useState<number | null>(null);
+  const [birthDate, setBirthDate] = useState<Date | undefined>(undefined);
+  const [showSuggestions, setShowSuggestions] = useState(false);
+  const debouncedCitySearchValue = useDebounce(city as string, 1000);
+  const [loading, setLoading] = useState(false);
 
-    useEffect(() => {
-        if (!settingsContext?.settings) return;
+  useEffect(() => {
+    if (!settingsContext?.settings) return;
 
-        setCity(settingsContext.settings.city ?? '');
-        setLongitude(settingsContext.settings.longitude ?? null);
-        setLatitude(settingsContext.settings.latitude ?? null);
-        setGender(settingsContext.settings.genderSelf ?? null);
-        setBirthDate(
-            settingsContext.settings.birthDate ? new Date(settingsContext.settings.birthDate) : undefined
-        );
-        
-    }, [settingsContext?.settings]);
+    setCity(settingsContext.settings.city ?? '');
+    setLongitude(settingsContext.settings.longitude ?? null);
+    setLatitude(settingsContext.settings.latitude ?? null);
+    setGender(settingsContext.settings.genderSelf ?? null);
+    setBirthDate(settingsContext.settings.birthDate ? new Date(settingsContext.settings.birthDate) : undefined);
+  }, [settingsContext?.settings]);
 
-    const handleCitySelect = async (city: City) => {
-        setShowSuggestions(false);
-        setCity(city.name);
-        setLongitude(city.longitude);
-        setLatitude(city.latitude);
-    };
+  const handleCitySelect = async (city: City) => {
+    setShowSuggestions(false);
+    setCity(city.name);
+    setLongitude(city.longitude);
+    setLatitude(city.latitude);
+  };
 
-    const handleCityInputChange = (value: string) => {
-        setCity(value);
-        setShowSuggestions(true);
-    };
+  const handleCityInputChange = (value: string) => {
+    setCity(value);
+    setShowSuggestions(true);
+  };
 
-    const handleUpdate = async () => {
-        if (!settingsContext?.settings) return;
+  const handleUpdate = async () => {
+    if (!settingsContext?.settings) return;
 
-        setLoading(true);
-        try {
-            if (!city || !latitude || !longitude || !birthDate || !gender)
-                return;
-            await userService.updateAttributesSettings({
-                city,
-                longitude,
-                latitude,
-                birth_date: birthDate.toISOString().split('T')[0],
-                gender_self: gender,
-            });
-            settingsContext.refreshSettings();
-            toast.success('Attributes updated successfully');
-        } catch (error) {
-            toast.error('Failed to update attributes');
-            console.error('Error updating attributes:', error);
-        } finally {
-            setLoading(false);
-        }
-    };
+    setLoading(true);
+    try {
+      if (!city || !latitude || !longitude || !birthDate || !gender) return;
+      await userService.updateAttributesSettings({
+        city,
+        longitude,
+        latitude,
+        birth_date: birthDate.toISOString().split('T')[0],
+        gender_self: gender,
+      });
+      await settingsContext.refreshSettings();
+      toast.success('Attributes updated successfully');
+    } catch (error) {
+      toast.error('Failed to update attributes');
+      console.error('Error updating attributes:', error);
+    } finally {
+      setLoading(false);
+    }
+  };
 
-    return (
-        <Card className="h-[475px] w-full border-none shadow-none">
-            <CardHeader>
-                <CardTitle>Attributes</CardTitle>
-                <CardDescription>Tell us about yourself</CardDescription>
-            </CardHeader>
-            <CardContent>
-                <form>
-                    <div className="grid w-full items-center gap-4">
-                        <div className="relative flex flex-col space-y-1.5">
-                            <Label htmlFor="name">Birth Date</Label>
-                            {birthDate !== undefined ? (
-                                <DatePicker
-                                    selectedDate={birthDate}
-                                    onDateChange={(dateString: string) =>
-                                        setBirthDate(new Date(dateString))
-                                    }
-                                />
-                            ) : (
-                                <Skeleton className="w=full h-[40px] bg-primary" />
-                            )}
-                        </div>
-                        <div className="relative flex flex-col space-y-1.5">
-                            <Label htmlFor="city">City</Label>
-                            {city !== undefined && city !== null ? (
-                                <InputField
-                                    type="text"
-                                    name="city"
-                                    placeholder="Enter your city"
-                                    value={city}
-                                    onChange={handleCityInputChange}
-                                    onFocus={() => setShowSuggestions(true)}
-                                    onBlur={() => {
-                                        setTimeout(() => {
-                                            setShowSuggestions(false);
-                                        }, 500);
-                                    }}
-                                />
-                            ) : (
-                                <Skeleton className="h-[40px] w-full" />
-                            )}
-                            <div
-                                className={`absolute top-full z-10 w-full ${!showSuggestions ? `hidden` : ``}`}
-                            >
-                                <CitySuggestions
-                                    searchTerm={debouncedCitySearchValue}
-                                    onCitySelect={handleCitySelect}
-                                    visible={showSuggestions}
-                                />
-                            </div>
-                        </div>
-                        <div className="flex flex-col space-y-1.5">
-                            <Label htmlFor="selfGender">Gender</Label>
-                            {gender !== null && genders !== null ? (
-                                <Select
-                                    value={gender?.toString()}
-                                    onValueChange={(value) =>
-                                        setGender(Number(value))
-                                    }
-                                >
-                                    <SelectTrigger id="selfGender">
-                                        <SelectValue placeholder="Select a gender..." />
-                                    </SelectTrigger>
-                                    <SelectContent position="popper">
-                                        {genders &&
-                                            genders.map((gender) => (
-                                                <SelectItem
-                                                    key={gender.id} // Add key prop
-                                                    value={gender.id.toString()}
-                                                >
-                                                    {gender.name}
-                                                </SelectItem>
-                                            ))}
-                                    </SelectContent>
-                                </Select>
-                            ) : (
-                                <Skeleton className="h-[40px] w-full rounded-md border border-[#e5e7eb]" />
-                            )}
-                        </div>
-                    </div>
-                </form>
-            </CardContent>
-            <CardFooter className="flex justify-end">
-                <Button onClick={handleUpdate} disabled={loading}>
-                    {loading ? (
-                        <>
-                            Updating <MotionSpinner />
-                        </>
-                    ) : (
-                        'Update'
-                    )}
-                </Button>
-            </CardFooter>
-        </Card>
-    );
+  return (
+    <Card className="h-[475px] w-full border-none shadow-none">
+      <CardHeader>
+        <CardTitle>Attributes</CardTitle>
+        <CardDescription>Tell us about yourself</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form>
+          <div className="grid w-full items-center gap-4">
+            <div className="relative flex flex-col space-y-1.5">
+              <Label htmlFor="name">Birth Date</Label>
+              {birthDate !== undefined ? (
+                <DatePicker
+                  selectedDate={birthDate}
+                  onDateChange={(dateString: string) => setBirthDate(new Date(dateString))}
+                />
+              ) : (
+                <Skeleton className="w=full h-[40px] bg-primary" />
+              )}
+            </div>
+            <div className="relative flex flex-col space-y-1.5">
+              <Label htmlFor="city">City</Label>
+              {city !== undefined && city !== null ? (
+                <InputField
+                  type="text"
+                  name="city"
+                  placeholder="Enter your city"
+                  value={city}
+                  onChange={handleCityInputChange}
+                  onFocus={() => setShowSuggestions(true)}
+                  onBlur={() => {
+                    setTimeout(() => {
+                      setShowSuggestions(false);
+                    }, 500);
+                  }}
+                />
+              ) : (
+                <Skeleton className="h-[40px] w-full" />
+              )}
+              <div className={`absolute top-full z-10 w-full ${!showSuggestions ? `hidden` : ``}`}>
+                <CitySuggestions
+                  searchTerm={debouncedCitySearchValue}
+                  onCitySelect={handleCitySelect}
+                  visible={showSuggestions}
+                />
+              </div>
+            </div>
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="selfGender">Gender</Label>
+              {gender !== null && genders !== null ? (
+                <Select value={gender?.toString()} onValueChange={(value) => setGender(Number(value))}>
+                  <SelectTrigger id="selfGender">
+                    <SelectValue placeholder="Select a gender..." />
+                  </SelectTrigger>
+                  <SelectContent position="popper">
+                    {genders &&
+                      genders.map((gender) => (
+                        <SelectItem
+                          key={gender.id} // Add key prop
+                          value={gender.id.toString()}
+                        >
+                          {gender.name}
+                        </SelectItem>
+                      ))}
+                  </SelectContent>
+                </Select>
+              ) : (
+                <Skeleton className="h-[40px] w-full rounded-md border border-[#e5e7eb]" />
+              )}
+            </div>
+          </div>
+        </form>
+      </CardContent>
+      <CardFooter className="flex justify-end">
+        <Button onClick={handleUpdate} disabled={loading}>
+          {loading ? (
+            <>
+              Updating <MotionSpinner />
+            </>
+          ) : (
+            'Update'
+          )}
+        </Button>
+      </CardFooter>
+    </Card>
+  );
 };
 
 export default UserAttributesCard;
--- front-end/src/pages/user-settings/components/UserProfileCard.tsx
+++ front-end/src/pages/user-settings/components/UserProfileCard.tsx
@@ -1,160 +1,139 @@
-import { useContext, useEffect, useState } from 'react';
+import { HOBBIES } from '@/assets/hobbies';
+import MotionSpinner from '@/components/animations/MotionSpinner';
 import { Button } from '@/components/ui/button';
-import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardFooter,
-    CardHeader,
-    CardTitle,
-} from '@/components/ui/card';
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
-import { SettingsContext } from '../SettingsContext';
-import { userService } from '@/features/user';
-import { toast } from 'sonner';
-import MotionSpinner from '@/components/animations/MotionSpinner';
-import { Skeleton } from '@/components/ui/skeleton';
-import { HOBBIES } from '@/assets/hobbies';
 import MultipleSelector, { Option } from '@/components/ui/multi-select';
+import { Skeleton } from '@/components/ui/skeleton';
+import { userService } from '@/features/user';
 import { hobbiesById } from '@/lib/utils/dataConversion';
-import ProfilePictureUploader from "@ui/forms/ProfilePictureUploader.tsx";
+import ProfilePictureUploader from '@ui/forms/ProfilePictureUploader.tsx';
+import { useContext, useEffect, useState } from 'react';
+import { toast } from 'sonner';
+import { SettingsContext } from '../SettingsContext';
 
 const UserProfileCard = () => {
-    const settingsContext = useContext(SettingsContext);
-    const [firstName, setFirstName] = useState<string | null>();
-    const [lastName, setLastName] = useState<string | null>();
-    const [hobbies, setHobbies] = useState<Option[] | undefined>([]);
-    const [alias, setAlias] = useState<string | null>();
-    const [loading, setLoading] = useState(false);
+  const settingsContext = useContext(SettingsContext);
+  const [firstName, setFirstName] = useState<string | null>();
+  const [lastName, setLastName] = useState<string | null>();
+  const [hobbies, setHobbies] = useState<Option[] | undefined>([]);
+  const [alias, setAlias] = useState<string | null>();
+  const [loading, setLoading] = useState(false);
 
-    useEffect(() => {
-        if (!settingsContext?.settings) return;
+  useEffect(() => {
+    if (!settingsContext?.settings) return;
 
-        setFirstName(settingsContext.settings.firstName ?? '');
-        setLastName(settingsContext.settings.lastName ?? '');
-        setAlias(settingsContext.settings.alias ?? '');
-        setHobbies(hobbiesById(settingsContext.settings.hobbies || []))
+    setFirstName(settingsContext.settings.firstName ?? '');
+    setLastName(settingsContext.settings.lastName ?? '');
+    setAlias(settingsContext.settings.alias ?? '');
+    setHobbies(hobbiesById(settingsContext.settings.hobbies || []));
+  }, [settingsContext?.settings]);
 
-    }, [settingsContext?.settings]);
+  const handleUpdate = async () => {
+    if (!settingsContext?.settings) return;
 
-    const handleUpdate = async () => {
-        if (!settingsContext?.settings) return;
+    setLoading(true);
+    try {
+      if (!firstName || !lastName || !alias) return;
+      await userService.updateProfileSettings({
+        first_name: firstName,
+        last_name: lastName,
+        alias,
+        hobbies: hobbies?.map((hobby) => parseInt(hobby.value)),
+      });
+      await settingsContext.refreshSettings();
+      toast.success('Profile updated successfully');
+    } catch (error) {
+      toast.error('Failed to update profile');
+      console.error('Error updating profile:', error);
+    } finally {
+      setLoading(false);
+    }
+  };
 
-        setLoading(true);
-        try {
-            if (!firstName || !lastName || !alias) return;
-            await userService.updateProfileSettings({
-                first_name: firstName,
-                last_name: lastName,
-                alias,
-                hobbies: hobbies?.map(hobby => parseInt(hobby.value))
-            });
-            settingsContext.refreshSettings();
-            toast.success('Profile updated successfully');
-        } catch (error) {
-            toast.error('Failed to update profile');
-            console.error('Error updating profile:', error);
-        } finally {
-            setLoading(false);
-        }
-    };
-
-    return (
-        <Card className="no-scrollbar h-[475px] w-full overflow-y-auto border-none shadow-none">
-            <CardHeader>
-                <CardTitle>Profile</CardTitle>
-                <CardDescription>
-                    Edit your profile settings here.
-                </CardDescription>
-            </CardHeader>
-            <CardContent>
-                <form>
-                    <div className="grid w-full items-center gap-4">
-                        <div className="flex gap-2">
-                            <div className="flex flex-col space-y-1.5">
-                                <Label htmlFor="firstName">First Name</Label>
-                                {firstName !== undefined && firstName !== null ? (
-                                    <Input
-                                        id="firstName"
-                                        placeholder="First Name"
-                                        value={firstName}
-                                        onChange={(e) =>
-                                            setFirstName(e.target.value)
-                                        }
-                                    />
-                                ) : (
-                                    <Skeleton className="h-[40px] w-full rounded-md border-[#e5e7eb]" />
-                                )}
-                            </div>
-                            <div className="flex flex-col space-y-1.5">
-                                <Label htmlFor="lastName">Last Name</Label>
-                                {lastName !== undefined && lastName !== null ? (
-                                    <Input
-                                        id="lastName"
-                                        placeholder="Last Name"
-                                        value={lastName}
-                                        onChange={(e) =>
-                                            setLastName(e.target.value)
-                                        }
-                                    />
-                                ) : (
-                                    <Skeleton className="h-[40px] w-full rounded-md border-[#e5e7eb]" />
-                                )}
-                            </div>
-                        </div>
-                        <div className="flex flex-col space-y-1.5">
-                            <label className="mb-1 text-sm font-medium text-gray-700">
-                                Profile Picture
-                            </label>
-                            <ProfilePictureUploader
-                                onUploadSuccess={() => {
-                                    console.debug('Upload was successful!');
-                                }}
-                            />
-                        </div>
-                        <div className="flex flex-col space-y-1.5">
-                            <Label htmlFor="alias">Alias</Label>
-                            {alias !== undefined && alias !== null ? (
-                                <Input
-                                    id="alias"
-                                    placeholder="Alias"
-                                    value={alias}
-                                    onChange={(e) => setAlias(e.target.value)}
-                                />
-                            ) : (
-                                <Skeleton className="h-[40px] w-full rounded-md border-[#e5e7eb]" />
-                            )}
-                        </div>
-                        <div className="flex flex-col space-y-1.5">
-                                <Label htmlFor="hobbies">Hobbies</Label>
-                                <MultipleSelector
-                                value={hobbies}
-                                onChange={setHobbies}
-                                placeholder='Select your hobbies...'
-                                defaultOptions={HOBBIES}
-                                groupBy='category'
-                                hideClearAllButton={true}
-                                maxSelected={5}
-                                hidePlaceholderWhenSelected={true}
-                                />
-                        </div>
-                    </div>
-                </form>
-            </CardContent>
-            <CardFooter className="flex justify-end mt-auto">
-                <Button onClick={handleUpdate} disabled={loading}>
-                    {loading ? (
-                        <>
-                            Updating <MotionSpinner />
-                        </>
-                    ) : (
-                        'Update'
-                    )}
-                </Button>
-            </CardFooter>
-        </Card>
-    );
+  return (
+    <Card className="no-scrollbar h-[475px] w-full overflow-y-auto border-none shadow-none">
+      <CardHeader>
+        <CardTitle>Profile</CardTitle>
+        <CardDescription>Edit your profile settings here.</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form>
+          <div className="grid w-full items-center gap-4">
+            <div className="flex gap-2">
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="firstName">First Name</Label>
+                {firstName !== undefined && firstName !== null ? (
+                  <Input
+                    id="firstName"
+                    placeholder="First Name"
+                    value={firstName}
+                    onChange={(e) => setFirstName(e.target.value)}
+                  />
+                ) : (
+                  <Skeleton className="h-[40px] w-full rounded-md border-[#e5e7eb]" />
+                )}
+              </div>
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="lastName">Last Name</Label>
+                {lastName !== undefined && lastName !== null ? (
+                  <Input
+                    id="lastName"
+                    placeholder="Last Name"
+                    value={lastName}
+                    onChange={(e) => setLastName(e.target.value)}
+                  />
+                ) : (
+                  <Skeleton className="h-[40px] w-full rounded-md border-[#e5e7eb]" />
+                )}
+              </div>
+            </div>
+            <div className="flex flex-col space-y-1.5">
+              <label className="mb-1 text-sm font-medium text-gray-700">Profile Picture</label>
+              <ProfilePictureUploader
+                onUploadSuccess={() => {
+                  console.debug('Upload was successful!');
+                }}
+              />
+            </div>
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="alias">Alias</Label>
+              {alias !== undefined && alias !== null ? (
+                <Input id="alias" placeholder="Alias" value={alias} onChange={(e) => setAlias(e.target.value)} />
+              ) : (
+                <Skeleton className="h-[40px] w-full rounded-md border-[#e5e7eb]" />
+              )}
+            </div>
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="hobbies">Hobbies</Label>
+              <MultipleSelector
+                value={hobbies}
+                onChange={setHobbies}
+                placeholder="Select your hobbies..."
+                defaultOptions={HOBBIES}
+                groupBy="category"
+                hideClearAllButton={true}
+                maxSelected={5}
+                hidePlaceholderWhenSelected={true}
+              />
+            </div>
+          </div>
+        </form>
+      </CardContent>
+      <CardFooter className="mt-auto flex justify-end">
+        <Button onClick={handleUpdate} disabled={loading}>
+          {loading ? (
+            <>
+              Updating <MotionSpinner />
+            </>
+          ) : (
+            'Update'
+          )}
+        </Button>
+      </CardFooter>
+    </Card>
+  );
 };
 
 export default UserProfileCard;
