8f4ffea141e92b41cd163710643558bdd8d17c1e
Karl Romet Somelar <karlromets@gmail.com>
2025-01-14 12:38:55 +0200

    refactor(controller): use constructor injection instead of field injection

--- back-end/src/main/java/com/matchme/srv/controller/GenderController.java
+++ back-end/src/main/java/com/matchme/srv/controller/GenderController.java
@@ -2,22 +2,22 @@ package com.matchme.srv.controller;
 
 import java.util.List;
 import java.util.stream.Collectors;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.matchme.srv.dto.response.GenderTypeDTO;
 import com.matchme.srv.repository.UserGenderTypeRepository;
+import lombok.RequiredArgsConstructor;
 
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/genders")
 public class GenderController {
 
-    @Autowired
-    private UserGenderTypeRepository genderRepository;
+    private final UserGenderTypeRepository genderRepository;
 
     @GetMapping()
     public List<GenderTypeDTO> getAllGenders() {
