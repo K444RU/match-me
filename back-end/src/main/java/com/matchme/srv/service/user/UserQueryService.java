41ed93bd3dbf02748f9edeac1ccf70d300f1c7b3
Karl Romet Somelar <108818570+karlromets@users.noreply.github.com>
2025-02-01 11:15:31 +0200

    refactor(service): remove impl

--- back-end/src/main/java/com/matchme/srv/service/user/UserQueryService.java
+++ back-end/src/main/java/com/matchme/srv/service/user/UserQueryService.java
@@ -5,20 +5,103 @@ import com.matchme.srv.dto.response.CurrentUserResponseDTO;
 import com.matchme.srv.dto.response.ProfileResponseDTO;
 import com.matchme.srv.dto.response.SettingsResponseDTO;
 import com.matchme.srv.dto.response.UserParametersResponseDTO;
+import com.matchme.srv.mapper.UserParametersMapper;
 import com.matchme.srv.model.user.User;
+import com.matchme.srv.model.user.UserAuth;
+import com.matchme.srv.model.user.profile.UserProfile;
+import com.matchme.srv.model.user.profile.user_attributes.UserAttributes;
+import com.matchme.srv.model.user.profile.user_preferences.UserPreferences;
+import com.matchme.srv.repository.UserRepository;
+import com.matchme.srv.service.AccessValidationService;
+import com.matchme.srv.service.user.dto.UserDTOMapper;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-public interface UserQueryService {
-  UserParametersResponseDTO getParameters(Long userId);
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class UserQueryService {
 
-  User getUser(Long userId);
+  private final UserRepository userRepository;
 
-  User getUserByEmail(String email);
+  private final AccessValidationService accessValidationService;
+  private final UserDTOMapper userDtoMapper;
 
-  CurrentUserResponseDTO getCurrentUserDTO(Long userId, Long targetUserId);
+  private final UserParametersMapper parametersMapper;
 
-  ProfileResponseDTO getUserProfileDTO(Long currentUserId, Long targetUserId);
+  private static final String USER_NOT_FOUND_MESSAGE = "User not found!";
+  private static final String PROFILE_NOT_FOUND_MESSAGE = "Profile not found!";
 
-  BiographicalResponseDTO getBiographicalResponseDTO(Long currentUserId, Long targetUserId);
+  /**
+   * Return a user’s basic info, first checks whether the current user can access the target’s data.
+   */
+  public CurrentUserResponseDTO getCurrentUserDTO(Long currentUserId, Long targetUserId) {
+    accessValidationService.validateUserAccess(currentUserId, targetUserId);
+    User user =
+        userRepository
+            .findById(targetUserId)
+            .orElseThrow(() -> new EntityNotFoundException(USER_NOT_FOUND_MESSAGE));
+    return userDtoMapper.toCurrentUserResponseDTO(user);
+  }
 
-  SettingsResponseDTO getSettingsResponseDTO(Long currentUserId, Long targetUserId);
+  public UserParametersResponseDTO getParameters(Long userId) {
+    User user =
+        userRepository
+            .findById(userId)
+            .orElseThrow(() -> new EntityNotFoundException(USER_NOT_FOUND_MESSAGE));
+    UserAuth auth = user.getUserAuth();
+    UserProfile profile = user.getProfile();
+    UserAttributes attributes = profile.getAttributes();
+    UserPreferences preferences = profile.getPreferences();
+
+    return parametersMapper.toUserParametersDTO(user, attributes, preferences, auth);
+  }
+
+  public User getUser(Long userId) {
+    return userRepository
+        .findById(userId)
+        .orElseThrow(() -> new EntityNotFoundException(USER_NOT_FOUND_MESSAGE));
+  }
+
+  public User getUserByEmail(String email) {
+    return userRepository
+        .findByEmail(email)
+        .orElseThrow(() -> new EntityNotFoundException(USER_NOT_FOUND_MESSAGE));
+  }
+
+  public ProfileResponseDTO getUserProfileDTO(Long currentUserId, Long targetUserId) {
+    accessValidationService.validateUserAccess(currentUserId, targetUserId);
+
+    User user = userRepository
+        .findById(targetUserId)
+        .orElseThrow(() -> new EntityNotFoundException(USER_NOT_FOUND_MESSAGE));
+
+    UserProfile profile = user.getProfile();
+    if (profile == null) {
+        throw new EntityNotFoundException(PROFILE_NOT_FOUND_MESSAGE);
+    }
+
+    return userDtoMapper.toProfileResponseDTO(profile);
+  }
+
+  public BiographicalResponseDTO getBiographicalResponseDTO(Long currentUserId, Long targetUserId) {
+    accessValidationService.validateUserAccess(currentUserId, targetUserId);
+
+    UserProfile profile =
+        userRepository
+            .findById(targetUserId)
+            .orElseThrow(() -> new EntityNotFoundException(USER_NOT_FOUND_MESSAGE))
+            .getProfile();
+    if (profile == null) throw new EntityNotFoundException(PROFILE_NOT_FOUND_MESSAGE);
+
+    return userDtoMapper.tobBiographicalResponseDTO(profile);
+  }
+
+  public SettingsResponseDTO getSettingsResponseDTO(Long currentUserId, Long targetUserId) {
+    accessValidationService.validateUserAccess(currentUserId, targetUserId);
+    UserParametersResponseDTO parameters = getParameters(targetUserId);
+    return userDtoMapper.toSettingsResponseDTO(parameters);
+  }
 }
--- back-end/src/main/java/com/matchme/srv/service/user/UserSettingsService.java
+++ back-end/src/main/java/com/matchme/srv/service/user/UserSettingsService.java
@@ -1,16 +1,143 @@
 package com.matchme.srv.service.user;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Service;
+
 import com.matchme.srv.dto.request.settings.AccountSettingsRequestDTO;
 import com.matchme.srv.dto.request.settings.AttributesSettingsRequestDTO;
 import com.matchme.srv.dto.request.settings.PreferencesSettingsRequestDTO;
 import com.matchme.srv.dto.request.settings.ProfileSettingsRequestDTO;
+import com.matchme.srv.exception.DuplicateFieldException;
+import com.matchme.srv.mapper.AttributesMapper;
+import com.matchme.srv.mapper.PreferencesMapper;
+import com.matchme.srv.model.user.User;
+import com.matchme.srv.model.user.profile.Hobby;
+import com.matchme.srv.model.user.profile.UserProfile;
+import com.matchme.srv.model.user.profile.user_attributes.UserAttributes;
+import com.matchme.srv.model.user.profile.user_preferences.UserPreferences;
+import com.matchme.srv.repository.UserRepository;
+import com.matchme.srv.service.HobbyService;
+import com.matchme.srv.service.type.UserGenderTypeService;
 
-public interface UserSettingsService {
-  void updateAccountSettings(Long userId, AccountSettingsRequestDTO settings);
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
-  void updateProfileSettings(Long userId, ProfileSettingsRequestDTO settings);
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class UserSettingsService {
 
-  void updateAttributesSettings(Long userId, AttributesSettingsRequestDTO settings);
+    private final UserRepository userRepository;
 
-  void updatePreferencesSettings(Long userId, PreferencesSettingsRequestDTO settings);
+    private final AttributesMapper attributesMapper;
+    private final PreferencesMapper preferencesMapper;
+
+    private final HobbyService hobbyService;
+    private final UserGenderTypeService userGenderTypeService;
+
+    private static final String USER_NOT_FOUND_MESSAGE = "User not found!";
+
+
+    /**
+     * Checks if the given email or phone number is used by a *different* user.
+     * If so, throws a DuplicateFieldException.
+     *
+     * @param email    - new email to be used
+     * @param number   - new phone number to be used
+     * @param userId   - the user doing the update (can be null for new users)
+     */
+    private void validateUniqueEmailAndNumber(String email, String number, Long userId) {
+
+        userRepository.findByEmailIgnoreCase(email)
+                .filter(existingUser -> !existingUser.getId().equals(userId))
+                .ifPresent(u -> {
+                throw new DuplicateFieldException("email", "Email already exists");
+                });
+
+        userRepository.findByNumber(number)
+                .filter(existingUser -> !existingUser.getId().equals(userId))
+                .ifPresent(u -> {
+                throw new DuplicateFieldException("number", "Phone number already exists");
+                });
+    }
+
+    public void updateAccountSettings(Long userId, AccountSettingsRequestDTO settings) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new EntityNotFoundException(USER_NOT_FOUND_MESSAGE));
+
+        log.info("Attempting to update account settings for userId: {}. New email: {}, new number: {}",
+        userId, settings.getEmail(), settings.getNumber());
+    
+        validateUniqueEmailAndNumber(settings.getEmail(), settings.getNumber(), user.getId());
+    
+        user.setEmail(settings.getEmail());
+        user.setNumber(settings.getNumber());
+    
+        userRepository.save(user);
+        log.info("Successfully updated user with ID: {}", userId);
+    }
+
+    public void updateProfileSettings(Long userId, ProfileSettingsRequestDTO settings) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new EntityNotFoundException(USER_NOT_FOUND_MESSAGE));
+
+        UserProfile profile = user.getProfile();
+        profile.setFirst_name(settings.getFirst_name());
+        profile.setLast_name(settings.getLast_name());
+        profile.setAlias(settings.getAlias());
+        if (settings.getHobbies() != null && !settings.getHobbies().isEmpty()) {
+            Set<Hobby> foundHobbies = settings.getHobbies().stream()
+                    .map(hobbyService::getById)
+                    .collect(Collectors.toSet());
+
+            profile.setHobbies(foundHobbies);
+        } else {
+            profile.setHobbies(new HashSet<>());
+        }
+
+        // TODO: Add logging
+        userRepository.save(user);
+    }
+
+    public void updateAttributesSettings(Long userId, AttributesSettingsRequestDTO settings) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new EntityNotFoundException(USER_NOT_FOUND_MESSAGE));
+
+        UserProfile profile = user.getProfile();
+        UserAttributes attributes = profile.getAttributes();
+
+        attributesMapper.toEntity(attributes, settings);
+        attributes.setGender(userGenderTypeService.getById(settings.getGender_self()));
+        attributes.setLocation(List.of(settings.getLongitude(), settings.getLatitude()));
+        attributes.setBirth_date(settings.getBirth_date());
+
+        profile.setCity(settings.getCity());
+
+        // TODO: Add logging
+        userRepository.save(user);
+    }
+
+    public void updatePreferencesSettings(Long userId, PreferencesSettingsRequestDTO settings) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new EntityNotFoundException(USER_NOT_FOUND_MESSAGE));
+
+        UserProfile profile = user.getProfile();
+        UserPreferences preferences = profile.getPreferences();
+
+        preferencesMapper.toEntity(preferences, settings);
+        preferences.setGender(userGenderTypeService.getById(settings.getGender_other()));
+        preferences.setAge_min(settings.getAge_min());
+        preferences.setAge_max(settings.getAge_max());
+        preferences.setDistance(settings.getDistance());
+        preferences.setProbability_tolerance(settings.getProbability_tolerance());
+
+        // TODO: Add logging
+        userRepository.save(user);
+    }
+
 }
