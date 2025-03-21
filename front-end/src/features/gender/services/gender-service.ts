b5c9bcf50b7754c653961a760725c4e9ee05e287
Karl Romet Somelar <108818570+karlromets@users.noreply.github.com>
2025-03-21 08:36:45 +0200

    fix(gender-service): update gender controller import to use user gender type controller

--- front-end/src/features/gender/services/gender-service.ts
+++ front-end/src/features/gender/services/gender-service.ts
@@ -1,24 +1,22 @@
-import { getGenderController } from '@/api/gender-controller';
 import { GenderTypeDTO } from '@/api/types';
+import { getUserGenderTypeController } from '@/api/user-gender-type-controller';
 
-const genderController = getGenderController();
+const genderController = getUserGenderTypeController();
 
 export const genderService = {
   getGenders: async (): Promise<GenderTypeDTO[]> => {
     try {
-      console.debug('🖖 GenderService: Making request')
+      console.debug('🖖 GenderService: Making request');
       const token = localStorage.getItem('authToken');
-      const response = await genderController.getAllGenders(
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        }
-      );
+      const response = await genderController.getAllGenders({
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
       return response.data;
     } catch (error) {
       console.error('❌ Error fetching genders', error);
       throw error;
     }
-  }
-}
+  },
+};
