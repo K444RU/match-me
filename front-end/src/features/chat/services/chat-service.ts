9bc2eebebdf2e368350e56e37efee8331c7ed91b
Karl Romet Somelar <108818570+karlromets@users.noreply.github.com>
2025-03-24 20:02:32 +0200

    feat(chat): add getChatPreviews

--- front-end/src/features/chat/services/chat-service.ts
+++ front-end/src/features/chat/services/chat-service.ts
@@ -1,7 +1,27 @@
+import { getChatController } from '@/api/chat-controller';
 import { ChatPreviewResponseDTO } from '@/api/types';
 import { ChatPreview } from '@/types/api';
 
+const chatController = getChatController();
+
 export const chatService = {
+  getChatPreviews: async () => {
+    try {
+      console.debug('🖖 ChatService: Making request');
+      const token = localStorage.getItem('authToken');
+      const response = await chatController.getChatPreviews({
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      console.debug('🖖 ChatService: Response', response.data);
+      return response.data;
+    } catch (error) {
+      console.error('❌ Error fetching chat previews', error);
+      throw error;
+    }
+  },
+
   sendMessage: (message: string, to: number, from: string) => {
     console.log(message, to, from);
   },
