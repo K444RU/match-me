e22ef1076fc678afeb340ce39e22c082560b7dac
Karl Romet Somelar <108818570+karlromets@users.noreply.github.com>
2025-03-24 17:07:54 +0200

    fix: update import paths for AuthProvider and AuthContext

--- front-end/src/features/authentication/hooks/use-auth.tsx
+++ front-end/src/features/authentication/hooks/use-auth.tsx
@@ -1,5 +1,5 @@
 import { useContext } from 'react';
-import { AuthContext, AuthContextType } from '../auth-context';
+import { AuthContext, AuthContextType } from '../context/auth-context';
 
 export const useAuth = (): AuthContextType => {
   return useContext(AuthContext);
--- front-end/src/features/authentication/index.ts
+++ front-end/src/features/authentication/index.ts
@@ -3,4 +3,4 @@ export { default as RegisterPage } from './components/RegisterPage';
 export { default as Login } from './components/PopUpForm';
 export { useAuth } from './hooks/use-auth';
 export { authService } from './services/auth-service';
-export { AuthProvider } from './auth-provider';
+export { AuthProvider } from './context/auth-provider';
