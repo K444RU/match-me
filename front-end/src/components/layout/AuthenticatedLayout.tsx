8ae834610b1f6d18e5410f612ab3a955be57c6b8
Karl Romet Somelar <108818570+karlromets@users.noreply.github.com>
2025-03-25 20:37:33 +0200

    refactor(AuthenticatedLayout): replace WebSocketProvider with ChatProvider for improved chat integration and update WebSocket URL format

--- front-end/src/components/layout/AuthenticatedLayout.tsx
+++ front-end/src/components/layout/AuthenticatedLayout.tsx
@@ -1,25 +1,22 @@
 import { useAuth } from '@/features/authentication';
-import { WebSocketProvider } from '@/features/chat/websocket-provider';
-import { FC } from 'react';
+import { ChatProvider } from '@/features/chat/';
 import { Outlet } from 'react-router-dom';
 import { SidebarProvider } from '../ui/sidebar';
 
-const WS_URL = import.meta.env.VITE_WS_URL || 'http://localhost:8000/ws';
+const WS_URL = import.meta.env.VITE_WS_URL || 'ws://localhost:8000/ws';
 
-const AuthenticatedLayout: FC = () => {
+export default function AuthenticatedLayout() {
   const { user } = useAuth();
 
   if (!user) return;
 
   return (
-    <WebSocketProvider wsUrl={WS_URL} token={user?.token}>
+    <ChatProvider wsUrl={WS_URL}>
       <SidebarProvider>
         <div className="flex h-screen w-screen overflow-hidden">
           <Outlet />
         </div>
       </SidebarProvider>
-    </WebSocketProvider>
+    </ChatProvider>
   );
-};
-
-export default AuthenticatedLayout;
+}
