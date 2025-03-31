7b0a25bd29c03ecbf66fa8c7f62632b81b8e2658
Karl Romet Somelar <108818570+karlromets@users.noreply.github.com>
2025-03-31 18:47:39 +0300

    refactor(me-service): remove unnecessary token handling in user parameter requests

--- front-end/src/features/user/services/me-service.ts
+++ front-end/src/features/user/services/me-service.ts
@@ -1,41 +1,27 @@
-import { CurrentUserResponseDTO, SettingsResponseDTO } from '@/api/types';
 import { getMeController } from '@/api/me-controller';
+import { CurrentUserResponseDTO, SettingsResponseDTO } from '@/api/types';
 
 const meController = getMeController();
 
 export const meService = {
-    getUserParameters: async (): Promise<SettingsResponseDTO> => {
-        console.log('📡 Fetching user parameters via JWT');
-        try {
-            const token = localStorage.getItem('authToken');
-            console.log('🛡️ Token used in request: ', token);
-            const response = await meController.getParameters({
-                headers: {
-                    Authorization: `Bearer ${token}`,
-                },
-            });
+  getUserParameters: async (): Promise<SettingsResponseDTO> => {
+    console.log('📡 Fetching user parameters via JWT');
+    try {
+      const response = await meController.getParameters();
+      return response;
+    } catch (error) {
+      console.error('❌ Error fetching user parameters', error);
+      throw error;
+    }
+  },
 
-            const mappedData: SettingsResponseDTO = response.data;
-            console.log('✅ Received user parameters: ', mappedData);
-            return mappedData;
-        } catch (error) {
-            console.error('❌ Error fetching user parameters', error);
-            throw error;
-        }
-    },
-
-    getCurrentUser: async (): Promise<CurrentUserResponseDTO> => {
-        try {
-            const token = localStorage.getItem('authToken');
-            const response = await meController.getCurrentUser({
-                headers: {
-                    Authorization: `Bearer ${token}`,
-                },
-            });
-            return response.data;
-        } catch (error) {
-            console.error('❌ Error fetching user parameters', error);
-            throw error;
-        }
-    },
+  getCurrentUser: async (): Promise<CurrentUserResponseDTO> => {
+    try {
+      const response = await meController.getCurrentUser();
+      return response;
+    } catch (error) {
+      console.error('❌ Error fetching user parameters', error);
+      throw error;
+    }
+  },
 };
