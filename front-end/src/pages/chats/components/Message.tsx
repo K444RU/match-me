a34269ea8634b91fdd8c830214f6d6c419a0bcef
Karl Romet Somelar <108818570+karlromets@users.noreply.github.com>
2025-04-13 11:37:09 +0300

    fix(Message): time -> message -> read state

--- front-end/src/pages/chats/components/Message.tsx
+++ front-end/src/pages/chats/components/Message.tsx
@@ -18,11 +18,11 @@ export default function Message({ message, isOwn = true, isLastMessage }: Messag
           </>
         )}
         <div className="flex flex-col items-end gap-1">
-          <div className={`size-fit rounded-md p-2 px-4 ${isOwn ? 'bg-primary-600 text-white' : 'bg-background-200'}`}>
-            {message.content}
-          </div>
           <div className="flex justify-end">
             <span className="text-xs">{format(fromUnixTime(Number(message.createdAt)), 'kk:mm')}</span>
+          </div>
+          <div className={`size-fit rounded-md p-2 px-4 ${isOwn ? 'bg-primary-600 text-white' : 'bg-background-200'}`}>
+            {message.content}
           </div>
           {isLastMessage && <MessageStatus eventType={message.event.type} isOwn={isOwn} />}
         </div>
