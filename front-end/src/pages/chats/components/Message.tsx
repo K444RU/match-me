975d9181b36e8aaa86b52f59e4362352021c0d06
Karl Romet Somelar <108818570+karlromets@users.noreply.github.com>
2025-04-13 12:52:16 +0300

    fix(Message): improve message sender alias and timestamp display

--- front-end/src/pages/chats/components/Message.tsx
+++ front-end/src/pages/chats/components/Message.tsx
@@ -13,14 +13,17 @@ export default function Message({ message, isOwn = true, isLastMessage }: Messag
     <div className={`flex w-full ${isOwn ? 'justify-end' : ''}`}>
       <div className={`mt-2 flex max-w-[50%] flex-col ${isOwn ? ' pl-4' : 'pr-4'}`}>
         {!isOwn && (
-          <>
-            <span>{message.senderAlias}</span>
-          </>
-        )}
-        <div className="flex flex-col items-end gap-1">
-          <div className="flex justify-end">
+          <div className="flex justify-between px-1 pb-1">
+            <span className="text-xs">{message.senderAlias}</span>
             <span className="text-xs">{format(fromUnixTime(Number(message.createdAt)), 'kk:mm')}</span>
           </div>
+        )}
+        <div className="flex flex-col items-end gap-1">
+          {isOwn && (
+            <div className="flex justify-end">
+              <span className="text-xs">{format(fromUnixTime(Number(message.createdAt)), 'kk:mm')}</span>
+            </div>
+          )}
           <div className={`size-fit rounded-md p-2 px-4 ${isOwn ? 'bg-primary-600 text-white' : 'bg-background-200'}`}>
             {message.content}
           </div>
