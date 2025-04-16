86dc1cda345ed21d8714462dee1106ad5952cb58
Karl Romet Somelar <108818570+karlromets@users.noreply.github.com>
2025-04-16 07:50:23 +0300

    refactor: move themeprovider to app

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,12 +1,15 @@
 import { AuthProvider } from '@/features/authentication';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './router/router';
+import { ThemeProvider } from './components/theme-provider';
 
 const App = () => {
   return (
+    <ThemeProvider>
     <AuthProvider>
       <RouterProvider router={router} future={{ v7_startTransition: true }} />
-    </AuthProvider>
+      </AuthProvider>
+    </ThemeProvider>
   );
 };
 
--- front-end/src/components/layout/MainLayout.tsx
+++ front-end/src/components/layout/MainLayout.tsx
@@ -1,13 +1,12 @@
 import { Toaster } from '@/components/ui/sonner';
 import { Outlet } from 'react-router-dom';
-import { ThemeProvider } from '../ThemeProvider';
 
 const MainLayout = () => {
   return (
-    <ThemeProvider>
+    <>
       <Outlet />
       <Toaster className="bg-black text-white" />
-    </ThemeProvider>
+    </>
   );
 };
 
