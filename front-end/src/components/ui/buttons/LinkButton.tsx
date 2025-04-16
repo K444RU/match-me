f19675fcf126bd4cd8dc92a0940694cecf77374d
Karl Romet Somelar <108818570+karlromets@users.noreply.github.com>
2025-04-16 20:39:37 +0300

    style: update LinkButton component styles for improved hover effect and background

--- front-end/src/components/ui/buttons/LinkButton.tsx
+++ front-end/src/components/ui/buttons/LinkButton.tsx
@@ -13,7 +13,7 @@ const LinkButton = ({ to, content }: LinkButtonProps) => {
   return (
     <MotionLink
       to={to}
-      className="focus-visible:ring-ring text-primary-foreground inline-flex h-10 items-center justify-center gap-2 whitespace-nowrap rounded-md bg-primary-50 px-4 py-2 text-sm font-semibold tracking-wide ring-offset-background transition-colors duration-300 focus-visible:outline-hidden focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0"
+      className="focus-visible:ring-ring bg-background text-primary-foreground shadow-xs hover:bg-primary/90 ring-offset-background focus-visible:outline-hidden inline-flex h-10 items-center justify-center gap-2 whitespace-nowrap rounded-md px-4 py-2 text-sm font-semibold tracking-wide transition-colors duration-300 focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 "
       whileHover={{
         scale: 1.1,
       }}
