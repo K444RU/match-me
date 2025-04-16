97532b96770b33bf03caa33ab5d531ea36dfff9e
Karl Romet Somelar <108818570+karlromets@users.noreply.github.com>
2025-04-16 20:43:25 +0300

    style: adjust FormLayout component for improved layout and visual consistency

--- front-end/src/features/authentication/components/FormLayout.tsx
+++ front-end/src/features/authentication/components/FormLayout.tsx
@@ -8,9 +8,9 @@ interface FormLayoutProps {
 const FormLayout = ({ title, children }: FormLayoutProps) => {
   return (
     <>
-      <div className="flex h-screen items-center justify-center bg-background">
-        <div className="w-full max-w-md rounded-md bg-accent-200 p-6">
-          <h1 className="mb-6 text-center text-3xl font-bold text-text">
+      <div className="flex h-full items-center justify-center">
+        <div className="bg-accent/50 mx-4 w-full max-w-md rounded-md p-6 sm:mx-0">
+          <h1 className="mb-6 text-center text-3xl font-bold">
             {title}
           </h1>
           {children}
