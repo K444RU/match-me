db96d8ce0386d49094e123c769253086aa3d2400
Karl Romet Somelar <108818570+karlromets@users.noreply.github.com>
2025-04-16 20:41:47 +0300

    refactor: enhance NavigationBarLayout structure for better layout management and responsiveness

--- front-end/src/components/layout/NavigationBarLayout.tsx
+++ front-end/src/components/layout/NavigationBarLayout.tsx
@@ -3,10 +3,16 @@ import Navbar from '../Navbar';
 
 const NavigationBarLayout = () => {
   return (
-    <>
-      <Navbar />
-      <Outlet />
-    </>
+    <div className="flex h-screen flex-col">
+      {/* Navbar takes its necessary height */}
+      <Navbar className="h-20"/>
+      {/* This div wraps the page content and grows to fill remaining space */}
+      <div className="flex-1 overflow-hidden">
+        {' '}
+        {/* Prevents this container from scrolling */}
+        <Outlet />
+      </div>
+    </div>
   );
 };
 
