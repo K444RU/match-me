086af840479cac1a870f7f5270f95d76d73ad824
Karl Romet Somelar <108818570+karlromets@users.noreply.github.com>
2025-04-16 20:39:30 +0300

    refactor: replace button with Button component in PopUpForm

--- front-end/src/features/authentication/components/PopUpForm.tsx
+++ front-end/src/features/authentication/components/PopUpForm.tsx
@@ -3,41 +3,38 @@ import { useState } from 'react';
 import { IoClose } from 'react-icons/io5';
 import LoginForm from './LoginForm';
 import RegisterForm from './RegisterForm';
+import { Button } from '@/components/ui/button';
 
 const Login = ({ isLogin = false }) => {
-  const buttonClass = isLogin
-    ? 'bg-primary-50 text-text hover:bg-primary-200 hover:text-text  px-5 py-2  '
-    : 'bg-primary text-background  border-2 border-primary hover:text-primary hover:bg-transparent  px-5 py-2 ';
-
   const [showOverlay, setShowOverlay] = useState(false);
   return (
     <div>
-      <button
+      <Button
         onClick={() => setShowOverlay(true)}
-        className={`${buttonClass} rounded-md font-semibold tracking-wide transition-colors duration-300`}
+        className={`font-semibold tracking-wide`}
       >
         {isLogin ? 'Log in' : 'Create account'}
-      </button>
+      </Button>
       <AnimatePresence>
         {showOverlay && (
           <motion.div exit={{ opacity: 0 }} className="fixed inset-0 z-50 flex items-center justify-center">
             <motion.div
               initial={{ opacity: 0 }}
               animate={{ opacity: 1 }}
-              className="absolute inset-0 bg-black/50 backdrop-blur-xs"
+              className="backdrop-blur-xs absolute inset-0 bg-black/50"
               onClick={() => setShowOverlay(false)}
             ></motion.div>
             <motion.div
               initial={{ scale: 0.3 }}
               animate={{ scale: 1 }}
               exit={{ scale: 0 }}
-              className="relative z-10 w-full max-w-xs rounded-lg bg-primary-50 p-6 shadow-lg"
+              className="bg-primary-50 relative z-10 w-full max-w-xs rounded-lg p-6 shadow-lg"
             >
               <h2 className="mb-3 w-full text-center text-2xl font-semibold">
                 {isLogin ? 'Log in' : 'Create account'}
               </h2>
               <IoClose
-                className="absolute -right-3 -top-3 size-8 rounded-3xl bg-primary-300 p-1.5 text-primary-50 transition-colors hover:cursor-pointer hover:bg-primary-400"
+                className="bg-primary text-primary-foreground hover:bg-primary/50 absolute -right-3 -top-3 size-8 rounded-3xl p-1.5"
                 onClick={() => setShowOverlay(false)}
               />
               {/* <p className="text-text-500">Enter your login details</p> */}
