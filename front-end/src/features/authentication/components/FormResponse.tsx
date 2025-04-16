e7896ab77a6a4fe6bb145bb587be12ebc6d7988e
Karl Romet Somelar <108818570+karlromets@users.noreply.github.com>
2025-04-16 20:39:19 +0300

    style: update FormResponse component styles for error state

--- front-end/src/features/authentication/components/FormResponse.tsx
+++ front-end/src/features/authentication/components/FormResponse.tsx
@@ -11,12 +11,12 @@ const FormResponse = ({
     <div
       className={`${
         state === 'error'
-          ? 'border-red-800 bg-red-200'
+          ? 'border-destructive bg-destructive/20'
           : 'border-green-800 bg-green-200'
       } flex w-full flex-col items-center justify-center border p-2`}
     >
       <h2 className="text-base font-semibold md:text-sm">{title}</h2>
-      <p className="text-sm text-text-500 md:text-xs">{subtitle}</p>
+      <p className="text-sm md:text-xs">{subtitle}</p>
     </div>
   );
 };
