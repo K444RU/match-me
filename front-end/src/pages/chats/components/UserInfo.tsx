0184da05088057b49f0ae02c87ec34070aa23974
Karl Romet Somelar <108818570+karlromets@users.noreply.github.com>
2025-04-19 18:00:04 +0300

    refactor: integrate UserAvatar component in UserInfo for improved styling and structure

--- front-end/src/pages/chats/components/UserInfo.tsx
+++ front-end/src/pages/chats/components/UserInfo.tsx
@@ -1,4 +1,3 @@
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -10,47 +9,26 @@ import {
 } from '@/components/ui/dropdown-menu';
 import { SidebarMenuButton } from '@/components/ui/sidebar';
 import { useAuth } from '@/features/authentication';
-import type { User as AuthUser } from '@/features/authentication';
 import { ChevronsUpDown, LogOut, Settings, User } from 'lucide-react';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import SettingsDialog from './SettingsDialog';
+import UserAvatar from './UserAvatar';
 
-const UserInfo = () => {
-  // Dunno how to see loading state since we are not awaiting useAuth...
-  // const [loading, setLoading] = useState<boolean>(true);
-  // const [error, setError] = useState<string | null>(null);
-  // Maybe we don't need it anyway
-
+export default function UserInfo() {
   const { user, logout } = useAuth();
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
   const [isDialogOpen, setIsDialogOpen] = useState(false);
   const navigate = useNavigate();
   if (!user) return;
 
-  const profileSrc = user.profilePicture;
-
-  function getInitials(user: AuthUser) {
-    if (!user.alias) {
-      return user.firstName ? user.firstName.charAt(0).toUpperCase() : user.lastName ? user.lastName.charAt(0).toUpperCase() : 'X';
-    } else {
-      return user.alias.charAt(0).toUpperCase();
-    }
-  }
-
   return (
     <>
       <DropdownMenu open={isDropdownOpen} onOpenChange={setIsDropdownOpen}>
         <DropdownMenuTrigger asChild>
           <div>
-            <SidebarMenuButton
-              size="lg"
-              className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
-            >
-              <Avatar className="size-8 rounded-lg">
-                <AvatarImage src={profileSrc} alt={user.firstName} />
-                <AvatarFallback className="rounded-lg">{getInitials(user)}</AvatarFallback>
-              </Avatar>
+            <SidebarMenuButton size="lg">
+              <UserAvatar name={user.firstName} profileSrc={user.profilePicture} />
               <div className="grid flex-1 text-left text-sm leading-tight">
                 <span className="truncate font-semibold">
                   {user.firstName} {user.lastName}
@@ -63,18 +41,16 @@ const UserInfo = () => {
         </DropdownMenuTrigger>
         <DropdownMenuContent
           className="w-(--radix-dropdown-menu-trigger-width) min-w-56 rounded-lg"
-          side="bottom"
+          side="right"
           align="end"
           sideOffset={4}
+          alignOffset={8}
         >
           <DropdownMenuLabel className="p-0 font-normal">
             <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
-              <Avatar className="size-8 rounded-lg">
-                <AvatarImage src={profileSrc} alt={user.firstName} />
-                <AvatarFallback className="rounded-lg text-sidebar-accent-foreground">{getInitials(user)}</AvatarFallback>
-              </Avatar>
+              <UserAvatar name={user.firstName} profileSrc={user.profilePicture} />
               <div className="grid flex-1 text-left text-sm leading-tight">
-                <span className="truncate font-semibold">
+                <span className="truncate">
                   {user.firstName} {user.lastName}
                 </span>
                 <span className="truncate text-xs">{user.alias}</span>
@@ -113,6 +89,4 @@ const UserInfo = () => {
       <SettingsDialog setIsOpen={setIsDialogOpen} isOpen={isDialogOpen} />
     </>
   );
-};
-
-export default UserInfo;
+}
